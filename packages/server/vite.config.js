acc9b3bf41c05ce2dbae266a26881f7c4d04d678
Bart Veneman <bart@frameless.io>
2025-10-02 13:43:11 +0200

    feat: package voor server

---
+++ packages/server/vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/get-css.ts"),
+			formats: ["es"],
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: [
+				"css-tree",
+				"linkedom"
+			],
+		},
+	},
+	plugins: [
+		dts(),
+	],
+})
