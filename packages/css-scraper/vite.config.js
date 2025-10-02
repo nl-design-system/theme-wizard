c46d1cdc3e54fc7743727750203675000baa6780
Bart Veneman <bart@frameless.io>
2025-10-02 14:18:40 +0200

    move scraper to standalone package

--- packages/css-scraper/vite.config.js
+++ packages/css-scraper/vite.config.js
@@ -5,10 +5,16 @@ import dts from "vite-plugin-dts"
 export default defineConfig({
 	build: {
 		lib: {
-			entry: resolve("./src/index.ts"),
+			entry: resolve("./src/get-css.ts"),
 			formats: ["es"],
 		},
 		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: [
+				"css-tree",
+				"linkedom"
+			],
 		},
 	},
 	plugins: [
