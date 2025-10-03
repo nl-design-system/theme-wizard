ab043f5065439378a7d0f6444b31582db9c7895d
Michelle <michelleermen@gmail.com>
2025-10-03 17:28:36 +0200

    fix: stencil workspace package import

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  publicDir: 'build',
+  publicDir: 'public',
+  devToolbar: {
+    enabled: false,
+  },
 });
