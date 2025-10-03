9239ee6e62b810a2211458f7bd78b21c96450da2
Michelle <michelleermen@gmail.com>
2025-10-03 13:49:28 +0200

    chore: set output to static

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
-  output: 'server',
   publicDir: 'build',
 });
