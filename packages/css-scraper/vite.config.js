62705a183e7de6204ce74d1cdd06a8cd5ed7ebee
Bart Veneman <bartveneman@gmail.com>
2025-10-07 15:34:14 +0200

    undo some csstree hacks

--- packages/css-scraper/vite.config.js
+++ packages/css-scraper/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  assetsInclude: ['**/*.json'], // for css-tree
   build: {
     lib: {
       entry: resolve('./src/get-css.ts'),
@@ -12,8 +11,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      // the .json is for css-tree because it requires .json files at runtime
-      external: ['css-tree', 'linkedom', /\.json$/],
+      external: ['linkedom'],
     },
   },
   plugins: [dts()],
