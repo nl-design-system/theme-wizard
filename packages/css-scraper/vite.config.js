adb4c896020479026ed1c1776250d37a45555286
Bart Veneman <bartveneman@gmail.com>
2025-10-07 14:49:29 +0200

    fix json imports for css-tree

--- packages/css-scraper/vite.config.js
+++ packages/css-scraper/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  assetsInclude: ['**/*.json'], // for css-tree
   build: {
     lib: {
       entry: resolve('./src/get-css.ts'),
@@ -11,7 +12,8 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['css-tree', 'linkedom'],
+      // the .json is for css-tree because it requires .json files at runtime
+      external: ['css-tree', 'linkedom', /\.json$/],
     },
   },
   plugins: [dts()],
