4cab910e66219f22a2d2eaccbbb6237832632ebe
Bart Veneman <bartveneman@gmail.com>
2025-10-09 12:32:16 +0200

    chore: extract server validation + error handling into separate function

--- packages/theme-wizard-server/src/index.ts
+++ packages/theme-wizard-server/src/index.ts
@@ -1,5 +1,5 @@
-import { getCss } from '@nl-design-system-community/css-scraper';
-import { Hono } from 'hono';
+import { getCss as getCssOrigins } from '@nl-design-system-community/css-scraper';
+import { type Context, type Next, Hono, MiddlewareHandler } from 'hono';
 import { cors } from 'hono/cors';
 import { HTTPException } from 'hono/http-exception';
 
@@ -16,32 +16,51 @@ app.get('/healthz', (c) => {
   return c.json({});
 });
 
-app.get('/api/get-css', async (c) => {
+export const requireUrlParam = async (c: Context, next: Next) => {
   const url = c.req.query('url');
 
   if (!url || url.trim().length === 0) {
-    throw new HTTPException(400, { message: 'missing `url` parameter: specify a url like ?url=example.com' });
+    throw new HTTPException(400, {
+      message: 'missing `url` parameter: specify a url like ?url=example.com',
+    });
   }
 
-  try {
+  await next();
+};
+
+export const withScrapingErrorHandler = (handler: MiddlewareHandler): MiddlewareHandler => {
+  return async (c: Context, next: Next) => {
+    try {
+      return await handler(c, next);
+    } catch (error) {
+      if (error instanceof Error && 'statusCode' in error) {
+        throw new HTTPException(400, {
+          cause: error,
+          message: error.message,
+        });
+      }
+      throw new HTTPException(500, {
+        cause: error,
+        message: 'encountered a scraping error',
+      });
+    }
+  };
+};
+
+app.get(
+  '/api/v1/css',
+  requireUrlParam,
+  withScrapingErrorHandler(async (c) => {
+    const url = c.req.query('url')!;
     const startTime = Date.now();
-    const origins = await getCss(url);
+    const origins = await getCssOrigins(url);
     const css = origins.map((origin) => origin.css).join('');
     const endTime = Date.now();
 
     c.res.headers.set('server-timing', `scraping;desc="Scraping CSS";dur=${endTime - startTime}`);
     c.res.headers.set('content-type', 'text/css; charset=utf-8');
     return c.body(css);
-  } catch (error: unknown) {
-    if (error instanceof Error && 'statusCode' in error) {
-      throw new HTTPException(400, {
-        cause: error,
-        message: error.message,
-      });
-    }
-
-    throw new HTTPException(500, { cause: error, message: 'encountered a scraping error' });
-  }
-});
+  }),
+);
 
 export default app;
