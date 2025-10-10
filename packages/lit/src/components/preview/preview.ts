291ac0d06407869a225b2506813918d3bc50840f
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-10 11:43:51 +0200

    WIP adopted stylesheets

--- packages/lit/src/components/preview/preview.ts
+++ packages/lit/src/components/preview/preview.ts
@@ -20,14 +20,6 @@ export class ThemePreview extends LitElement {
   @state() private isLoading = false;
   @state() private error = '';
 
-  override firstUpdated() {
-    if (this.shadowRoot) {
-      this.shadowRoot.adoptedStyleSheets = [this.stylesheet];
-    } else {
-      throw new Error('Adopted stylesheets only work on (shadow-)DOM');
-    }
-  }
-
   override willUpdate(changedProps: Map<string | number | symbol, unknown>) {
     // Fetch content when URL changes (before render)
     if (changedProps.has('url') && this.url) {
