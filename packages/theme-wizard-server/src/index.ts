5210cd90c6fc368c2c9adb26997cd596b80ef84f
Bart Veneman <bartveneman@gmail.com>
2025-10-13 14:39:16 +0200

    add vercel workaround

--- packages/theme-wizard-server/src/index.ts
+++ packages/theme-wizard-server/src/index.ts
@@ -1,9 +1,6 @@
 import { createRoute, OpenAPIHono, z } from '@hono/zod-openapi';
 import { getCss as getCssOrigins } from '@nl-design-system-community/css-scraper';
 import { css_to_tokens as cssToTokens } from '@projectwallace/css-design-tokens';
-// @ts-expect-error Vercel expects this import
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { timing, startTime, endTime } from 'hono/timing';
 import pkg from '../package.json';
