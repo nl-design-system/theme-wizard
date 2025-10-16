bd5b4cbb672e8dda8094aef37790589f1e6c2970
Bart Veneman <bartveneman@gmail.com>
2025-10-16 10:42:53 +0200

    let server use schema from css-scraper

--- packages/css-scraper/src/design-tokens.ts
+++ packages/css-scraper/src/design-tokens.ts
@@ -1,35 +1,18 @@
 import {
   css_to_tokens as cssToTokens,
-  type ColorToken as PWColortoken,
-  type FontFamilyToken as PWFontFamilyToken,
-  type DimensionToken as PWDimensionToken,
   EXTENSION_AUTHORED_AS as PW_EXTENSION_AUTHORED_AS,
   EXTENSION_CSS_PROPERTIES as PW_EXTENSION_CSS_PROPERTIES,
   EXTENSION_USAGE_COUNT as PW_EXTENSION_USAGE_COUNT,
 } from '@projectwallace/css-design-tokens';
+import {
+  EXTENSION_AUTHORED_AS,
+  EXTENSION_CSS_PROPERTIES,
+  EXTENSION_TOKEN_ID,
+  EXTENSION_USAGE_COUNT,
+  type DesignToken,
+} from './design-tokens.types';
 
-export const EXTENSION_TOKEN_ID = 'nl.nldesignsystem.theme-wizard.token-id';
-export const EXTENSION_USAGE_COUNT = 'nl.nldesignsystem.theme-wizard.usage-count';
-export const EXTENSION_AUTHORED_AS = 'nl.nldesignsystem.theme-wizard.css-authored-as';
-export const EXTENSION_CSS_PROPERTIES = 'nl.nldesignsystem.theme-wizard.css-properties';
-
-type TokenExtensions = {
-  [EXTENSION_TOKEN_ID]: string;
-  [EXTENSION_USAGE_COUNT]: number;
-  [EXTENSION_AUTHORED_AS]: string;
-  [EXTENSION_CSS_PROPERTIES]: string[];
-};
-
-type BaseToken = {
-  $extensions: TokenExtensions;
-};
-
-// Define all token types
-export type ColorToken = BaseToken & Pick<PWColortoken, '$type' | '$value'>;
-export type FontFamilyToken = BaseToken & Pick<PWFontFamilyToken, '$type' | '$value'>;
-export type FontSizeToken = BaseToken & Pick<PWDimensionToken, '$type' | '$value'>;
-
-export type DesignToken = ColorToken | FontFamilyToken | FontSizeToken;
+export * from './design-tokens.types';
 
 export const getDesignTokens = (css: string): DesignToken[] => {
   const tokens = cssToTokens(css);
