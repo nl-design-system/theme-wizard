43204dadf4b52da1b87c64540ab53f7b2378b87b
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-17 12:22:55 +0200

    chore(lit): allow basis namespace in css in order to remap them

--- packages/lit/.stylelintrc.mjs
+++ packages/lit/.stylelintrc.mjs
@@ -1,6 +1,13 @@
 export default {
   extends: ['../../.stylelintrc.json'],
   ignoreFiles: ['**/*', '!**/*.ts'],
+  rules: {
+    'custom-property-pattern': '^_?(basis|theme|denhaag|utrecht)-[a-z0-9-]+$',
+    'keyframes-name-pattern': '^(basis|theme|utrecht)-[a-z0-9-]+$',
+    'scss/dollar-variable-pattern': '^(basis|theme|utrecht)-[a-z0-9-]+$',
+    'scss/percent-placeholder-pattern': '^(basis|theme|utrecht)-[a-z0-9-]+$',
+    'selector-class-pattern': '^(basis|theme|denhaag|utrecht)-[a-z0-9_-]+$',
+  },
   overrides: [
     {
       customSyntax: 'postcss-lit',
