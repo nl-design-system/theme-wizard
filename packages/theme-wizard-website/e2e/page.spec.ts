4792108ef22e24df5584cde549f06836b532273b
Bart Veneman <bartveneman@gmail.com>
2025-10-21 13:05:03 +0200

    mock the /css route for debugging

--- packages/theme-wizard-website/e2e/page.spec.ts
+++ packages/theme-wizard-website/e2e/page.spec.ts
@@ -12,6 +12,12 @@ test('page has accessibility basics', async ({ page }) => {
 });
 
 test('can change heading font to Courier New', async ({ page }) => {
+  await page.route('http://localhost:9491/api/v1/css', async (route) => {
+    await route.fulfill({
+      body: 'a { color: blue; }',
+    });
+  });
+
   await page.goto('/');
 
   const preview = page.getByRole('main');
