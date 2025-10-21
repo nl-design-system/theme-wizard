c6e39a1104511ec5dd1e828841e5c7f977d23a70
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-21 16:11:51 +0200

    test(Scraper): do multiple scrape actions

--- packages/lit/src/lib/Scraper/index.test.ts
+++ packages/lit/src/lib/Scraper/index.test.ts
@@ -13,4 +13,15 @@ describe('Scraper', () => {
 
     expect(fetchSpy).toBeCalledWith(new URL(`${scraperURL}api/v1/css?url=${encodeURIComponent(targetURL)}`));
   });
+
+  test('can be invoked multiple times', () => {
+    const scraperURL = 'https://example.com/';
+    const targetURL = 'https://example.com/test';
+    const scraper = new Scraper(scraperURL);
+
+    scraper.getCSS(new URL(targetURL));
+    scraper.getCSS(new URL(targetURL));
+
+    expect(fetchSpy).toBeCalledWith(new URL(`${scraperURL}api/v1/css?url=${encodeURIComponent(targetURL)}`));
+  });
 });
