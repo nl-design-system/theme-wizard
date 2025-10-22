707d154d44a9df24019b846736edee16e6d7e5ab
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-22 10:24:34 +0200

    feat(ColorScale): initial interface

--- packages/lit/src/lib/ColorScale/index.ts
+++ packages/lit/src/lib/ColorScale/index.ts
@@ -1,5 +1,44 @@
 import ColorToken from '../ColorToken';
 
 export default class ColorScale {
-  constructor(token: ColorToken) {}
+  #from: ColorToken;
+  #fromOKLCH: ColorToken;
+  #derived: ColorToken[] = [];
+  #derivedOKLCH: ColorToken[] = [];
+  #size: number = 12;
+
+  constructor(from: ColorToken) {
+    this.from = from;
+    // Explicitly set in constructor to satisfy TypeScript
+    this.#from = this.from;
+    this.#fromOKLCH = this.fromOKLCH;
+  }
+
+  get from() {
+    return this.#from;
+  }
+
+  set from(value: ColorToken) {
+    const token = value instanceof ColorToken ? value : new ColorToken(value);
+    this.#from = token;
+    this.#fromOKLCH = token.toColorSpace('oklch');
+    this.#derivedOKLCH = [this.#fromOKLCH];
+    this.#derived = this.#derivedOKLCH.map((token) => token); // Convert back to `this.from` color space
+  }
+
+  get fromOKLCH() {
+    return this.#fromOKLCH;
+  }
+
+  get size() {
+    return this.#size;
+  }
+
+  list() {
+    return this.#derived;
+  }
+
+  get(index: number) {
+    return this.#derived[index];
+  }
 }
