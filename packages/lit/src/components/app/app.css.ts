a10af2c36dd5bb92c014d91ec887c1b0ec437406
Michelle <michelleermen@gmail.com>
2025-10-23 13:36:58 +0200

    fix: lint

--- packages/lit/src/components/app/app.css.ts
+++ packages/lit/src/components/app/app.css.ts
@@ -5,6 +5,7 @@ export default css`
    APP LAYOUT
    ============================================ */
   .theme-app {
+
     --basis-form-control-max-inline-size: unset;
     display: flex;
     min-block-size: 100vh;
