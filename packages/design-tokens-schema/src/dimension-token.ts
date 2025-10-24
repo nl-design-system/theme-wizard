5f7cd9a7a7300c2e7249f8269d692edf0ba59e57
Bart Veneman <bartveneman@gmail.com>
2025-10-24 11:45:05 +0200

    expose more low-level dimension unit

--- packages/design-tokens-schema/src/dimension-token.ts
+++ packages/design-tokens-schema/src/dimension-token.ts
@@ -3,8 +3,13 @@ import { BaseDesignTokenValueSchema } from './base-token';
 
 // 8.2 Dimension
 
+export const DimensionUnitSchema = z.union([z.literal('px'), z.literal('rem')]);
+
+/** @see https://www.designtokens.org/tr/drafts/format/#dimension */
+export type DimensionUnit = z.infer<typeof DimensionUnitSchema>;
+
 export const DimensionValueSchema = z.strictObject({
-  unit: z.union([z.literal('px'), z.literal('rem')]),
+  unit: DimensionUnitSchema,
   value: z.number(),
 });
 
