e13e99d36de0a6cd54e2e77559e10257b7d8fe31
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-27 10:53:33 +0100

    style(color-token): use uppercase for color space names

--- packages/design-tokens-schema/src/color-token.ts
+++ packages/design-tokens-schema/src/color-token.ts
@@ -4,29 +4,24 @@ import { BaseDesignTokenValueSchema } from './base-token';
 
 // 8.1 Color -> 4.1 Color Module: Format
 
-const spaces = [
-  'a98-rgb',
-  'display-p3',
-  'hsl',
-  'hwb',
-  'lab',
-  'lch',
-  'oklab',
-  'oklch',
-  'prophoto-rgb',
-  'rec2020',
-  'srgb',
-  'xyz-d50',
-  'xyz-d65',
-] as const;
-
 /** @see https://www.designtokens.org/tr/drafts/color/#supported-color-spaces */
-export const COLOR_SPACES = Object.fromEntries(spaces.map((space) => [space, space])) as Record<
-  (typeof spaces)[number],
-  (typeof spaces)[number]
->;
+export const COLOR_SPACES = {
+  A98_RGB: 'a98-rgb',
+  DISPLAY_P3: 'display-p3',
+  HSL: 'hsl',
+  HWB: 'hwb',
+  LAB: 'lab',
+  LCH: 'lch',
+  OKLAB: 'oklab',
+  OKLCH: 'oklch',
+  PROPHOTO_RGB: 'prophoto-rgb',
+  REC2020: 'rec2020',
+  SRGB: 'srgb',
+  XYZ_D50: 'xyz-d50',
+  XYZ_D65: 'xyz-d65',
+} as const;
 
-export const ColorSpaceSchema = z.union(spaces.map((space) => z.literal(space)));
+export const ColorSpaceSchema = z.union(Object.values(COLOR_SPACES).map((space) => z.literal(space)));
 
 export const NoneKeywordSchema = z.literal('none');
 
