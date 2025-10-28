183169d32ef245a002edbc16028419b8c93d11e4
Michelle <michelleermen@gmail.com>
2025-10-28 14:59:39 +0100

    feat: add page header from amsterdam css

---
+++ packages/lit/src/components/page-header/page-header.ts
@@ -0,0 +1,33 @@
+import amsStyles from '@amsterdam/design-system-css/dist/index.css?inline';
+import { LitElement, html, unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('theme-wizard-page-header')
+export class ThemeWizardPageHeader extends LitElement {
+  static override readonly styles = [unsafeCSS(amsStyles)];
+  override render() {
+    return html` <header class="ams-page-header">
+      <a class="ams-page-header__logo-link" href="/">
+        <span class="ams-page-header__logo-container">
+          <slot name="logo"></slot>
+        </span>
+      </a>
+
+      <nav aria-labelledby="primary-navigation" class="ams-page-header__navigation">
+        <h2 aria-hidden="true" class="ams-visually-hidden" id="primary-navigation">Hoofdnavigatie</h2>
+
+        <ul class="ams-page-header__menu">
+          <li class="ams-page-header__menu-item">
+            <slot name="menu-links"></slot>
+          </li>
+        </ul>
+      </nav>
+    </header>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'theme-wizard-page-header': ThemeWizardPageHeader;
+  }
+}
