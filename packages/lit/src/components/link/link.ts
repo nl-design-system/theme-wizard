ebff63c2704516088f2e298133b01d25750af781
Michelle <michelleermen@gmail.com>
2025-10-28 17:03:13 +0100

    style: remove bloat

--- packages/lit/src/components/link/link.ts
+++ packages/lit/src/components/link/link.ts
@@ -1,29 +1,12 @@
 import linkCss from '@nl-design-system-candidate/link-css/link.css?inline';
-import { css, html, LitElement, unsafeCSS } from 'lit';
+import { html, LitElement, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 @customElement('theme-wizard-link')
 export class ThemeWizardLink extends LitElement {
   @property() href: string = '';
 
-  static override readonly styles = [
-    unsafeCSS(linkCss),
-    css`
-      .nl-link {
-        text-decoration: none;
-        display: inline-flex;
-        align-items: center;
-        gap: 0.25rem;
-      }
-      .nl-link__label {
-        text-decoration: underline;
-      }
-      .nl-link__icon {
-        text-decoration: none;
-        display: inline-flex;
-      }
-    `,
-  ];
+  static override readonly styles = [unsafeCSS(linkCss)];
 
   override render() {
     return html`
