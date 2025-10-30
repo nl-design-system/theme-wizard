aa6e1e8b1893eba4bdfeb752cb3fb94e09b1cbe8
Michelle <michelleermen@gmail.com>
2025-10-30 17:22:40 +0100

    fix: lint

--- packages/lit/src/components/skip-link/styles.ts
+++ packages/lit/src/components/skip-link/styles.ts
@@ -2,6 +2,7 @@ import { css } from 'lit';
 
 export default css`
   /* Override position to absolute to move the position to in the preview */
+  /* stylelint-disable-next-line selector-class-pattern */
   .nl-skip-link--visible-on-focus:focus {
     position: absolute;
   }
