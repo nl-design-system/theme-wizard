40fc64de978b3f4ebe3911bf70e14fe91927cdbb
Jurgen Beliën <jurgen@voorhoede.nl>
2025-10-30 16:28:02 +0100

    style(ColorScale): make lightness mask readonly

--- packages/lit/src/lib/ColorScale/index.ts
+++ packages/lit/src/lib/ColorScale/index.ts
@@ -7,7 +7,7 @@ export default class ColorScale {
   #derived: ColorToken[] = [];
   #derivedOKLCH: ColorToken[] = [];
   // Mandatory high to low
-  static #lightnessMask = [0.999, 0.97, 0.95, 0.93, 0.91, 0.79, 0.56, 0.52, 0.48, 0.38, 0.32, 0.26, 0.14, 0.1];
+  static readonly #lightnessMask = [0.999, 0.97, 0.95, 0.93, 0.91, 0.79, 0.56, 0.52, 0.48, 0.38, 0.32, 0.26, 0.14, 0.1];
 
   constructor(from: ColorToken) {
     this.from = from;
