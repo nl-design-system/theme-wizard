cac4355f1c898469df24f629ab94b6c756babd19
Michelle <michelleermen@gmail.com>
2025-11-03 17:34:52 +0100

    chore: remove components

--- packages/lit/src/components/skip-link/styles.ts
+++
@@ -1,8 +0,0 @@
-import { css } from 'lit';
-
-export default css`
-  /* Override position to absolute to move the position to in the preview */
-  .nl-skip-link--visible-on-focus:focus {
-    position: absolute;
-  }
-`;
