2ffbe2b2ee71491be341d2abca72bfc6b116063f
Jurgen Beliën <jurgen@voorhoede.nl>
2025-11-03 16:16:43 +0100

    test(color-scale-picker): test behavior

--- packages/lit/src/components/color-scale-picker/index.test.ts
+++ packages/lit/src/components/color-scale-picker/index.test.ts
@@ -1,5 +1,6 @@
+import { BrandSchema } from '@nl-design-system-community/design-tokens-schema';
 import { beforeEach, describe, expect, test } from 'vitest';
-import { BrandSchema } from '@nl-design-system-community/design-tokens-schema'
+import { page } from 'vitest/browser';
 import './index';
 
 const tag = 'color-scale-picker';
@@ -25,5 +26,16 @@ describe(`<${tag}>`, () => {
     const element = document.querySelector(tag);
     const value = element?.value;
     expect(BrandSchema.safeParse(value)).toBeTruthy();
+  });
+
+  test('value updates when color changes', async () => {
+    const element = document.querySelector(tag);
+    /* @ts-expect-error element/shadowroot could be undefined, but it is fine if it would throw */
+    const color = page.elementLocator(element?.shadowRoot?.querySelector('input[type=color]'));
+    await color.fill('#990000');
+    const first = element?.value || {};
+    await color.fill('#000099');
+    const second = element?.value || {};
+    expect(first).not.toMatchObject(second);
   });
 });
