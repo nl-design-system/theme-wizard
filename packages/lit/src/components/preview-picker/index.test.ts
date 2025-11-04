2eb17b327835d69190c55744a51e599cb9d4ee01
Michelle <michelleermen@gmail.com>
2025-11-04 09:56:50 +0100

    fix: preview-picker unit test

--- packages/lit/src/components/preview-picker/index.test.ts
+++ packages/lit/src/components/preview-picker/index.test.ts
@@ -1,12 +1,30 @@
 import '.';
+import type { Category, TemplateGroup } from '@nl-design-system-community/templates';
 import { beforeEach, describe, expect, test } from 'vitest';
 import type { PreviewPicker } from '.';
 
 const tag = 'preview-picker';
+const category: Category = 'template';
+
+const templates = {
+  name: 'My Environment',
+  pages: [
+    {
+      name: 'Some beautiful page',
+      value: '/something-beautiful/some-beautiful-page',
+    },
+    {
+      name: 'Another beautiful page',
+      value: '/another-beautiful-thing/another-beautiful-page',
+    },
+  ],
+  type: category,
+};
 
 const mount = async () => {
   document.body.innerHTML = `<${tag}></${tag}>`;
   const el = document.querySelector(tag) as PreviewPicker;
+  el.templates = [templates] as TemplateGroup[];
   await el.updateComplete;
   return el;
 };
@@ -33,23 +51,23 @@ describe(`<${tag}>`, () => {
     const options = dropdown?.options;
     const allValues = options?.flatMap((g) => g.detail?.map((d) => d.value));
 
-    expect(allValues).toContain('/my-environment/overview');
-    expect(allValues).toContain('/preview-components/collage-1');
+    expect(allValues).toContain('/something-beautiful/some-beautiful-page');
+    expect(allValues).toContain('/another-beautiful-thing/another-beautiful-page');
   });
 
   test('defaults to first option when no templatePath param', async () => {
     const el = await mount();
     const dropdown = el.shadowRoot?.querySelector('wiz-dropdown');
 
-    expect(dropdown?.value).toBe('/my-environment/overview');
+    expect(dropdown?.value).toBe('/something-beautiful/some-beautiful-page');
   });
 
   test('reads current value from ?templatePath query param', async () => {
     const origin = globalThis.location.origin;
-    globalThis.history.pushState({}, '', `${origin}/?templatePath=/preview-components/collage-1`);
+    globalThis.history.pushState({}, '', `${origin}/?templatePath=/something-beautiful/some-beautiful-page`);
     const el = await mount();
     const dropdown = el.shadowRoot?.querySelector('wiz-dropdown');
 
-    expect(dropdown?.value).toBe('/preview-components/collage-1');
+    expect(dropdown?.value).toBe('/something-beautiful/some-beautiful-page');
   });
 });
