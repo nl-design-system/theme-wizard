7348d24023718e8dc78cbb7843c1a4667c01d648
Jurgen Beliën <jurgen@voorhoede.nl>
2025-11-06 15:33:17 +0100

    feat: add Persistent Storage helper class

---
+++ packages/lit/src/lib/PersistentStorage/index.ts
@@ -0,0 +1,79 @@
+const STORAGE_TYPES = {
+  LOCAL_STORAGE: 'localStorage',
+  SESSION_STORAGE: 'sessionStorage',
+} as const;
+
+type StorageType = (typeof STORAGE_TYPES)[keyof typeof STORAGE_TYPES];
+
+type Options = {
+  type?: StorageType;
+  prefix?: string;
+};
+export default class PersistentStorage {
+  #type: StorageType;
+  #backend: Storage;
+  #prefix: string;
+
+  constructor({ prefix = '', type: requestedType = STORAGE_TYPES.LOCAL_STORAGE }: Options = {}) {
+    let type = requestedType;
+    const backupType = STORAGE_TYPES.SESSION_STORAGE;
+
+    if (!PersistentStorage.#isSupported(type)) {
+      if (type !== backupType) {
+        console.error(`Cannot create persistent storage, no support for ${type}.`);
+      }
+      type = backupType;
+      if (!PersistentStorage.#isSupported(backupType)) {
+        throw new Error(`Cannot create persistent storage, no support for ${backupType}.`);
+      }
+    }
+
+    this.#prefix = prefix;
+    this.#type = type;
+    this.#backend = window[type];
+  }
+
+  get length() {
+    return this.#backend.length;
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  getItem(key: string) {
+    return this.#backend.getItem(`${this.#prefix}${key}`);
+  }
+
+  key(index: number) {
+    return this.#backend.key(index);
+  }
+
+  removeItem(key: string) {
+    return this.#backend.removeItem(`${this.#prefix}${key}`);
+  }
+
+  setItem(key: string, value: string) {
+    this.#backend.setItem(`${this.#prefix}${key}`, value);
+  }
+
+  /** @see: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API */
+  static #isSupported(type: StorageType) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === 'QuotaExceededError' &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+}
