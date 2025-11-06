dfe6c87534aabc6d5db20b51a7bad2447710c5f1
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-11-06 11:02:09 +0100

    feat: add color contrast to $extensions for known colors (#144)
    ```ts
    import { 
      ThemeSchema, 
      addContrastExtensions, 
      EXTENSION_CONTRAST_WITH, 
    } from '@nl-design-system-community/design-token-schema'
    
    ThemeSchema.transform(addContrastExtensions).safeParse(myTokens)
    ```
    
    This adds the following to `basis.color` tokens:
    
    ```ts
    $extensions: {
      [EXTENSION_CONTRAST_WITH]: [{
        color: {
          $type: 'color',
          $value: '{common.basis.color.default.bg-document}'
        },
        ratio: 4.5,
      }]
    }
    ```
    
    refs #67

--- packages/design-tokens-schema/src/basis-tokens.test.ts
+++ packages/design-tokens-schema/src/basis-tokens.test.ts
@@ -10,6 +10,8 @@ import {
   ThemeSchema,
   resolveConfigRefs,
   BasisTextSchema,
+  addContrastExtensions,
+  EXTENSION_CONTRAST_WITH,
 } from './basis-tokens';
 
 describe('brand', () => {
@@ -280,6 +282,75 @@ describe('theme', () => {
       },
     },
   };
+
+  describe('adding contrast-with extensions', () => {
+    test('adds contrast-with extensions for tokens that we know', () => {
+      const config = {
+        brand: brandConfig,
+        common: {
+          basis: {
+            color: {
+              default: {
+                'color-document': {
+                  $type: 'color',
+                  $value: `{ma.color.indigo.5}`,
+                  // the transformation will add an $extension here
+                },
+              },
+            },
+          },
+        },
+      };
+      const result = ThemeSchema.transform(addContrastExtensions).safeParse(config);
+      expect(result.success).toEqual(true);
+      expect(result.data?.common?.basis?.color?.default?.['color-document']?.$extensions).toMatchObject({
+        [EXTENSION_CONTRAST_WITH]: [
+          {
+            color: {
+              $type: 'color',
+              $value: '{common.basis.color.default.bg-subtle}',
+            },
+            ratio: 4.5,
+          },
+        ],
+      });
+    });
+  });
+
+  test('contrast-with extensions do not mess with existing extensions', () => {
+    const config = {
+      brand: brandConfig,
+      common: {
+        basis: {
+          color: {
+            default: {
+              'color-document': {
+                $extensions: {
+                  // the transformation will add an extra contrast color here
+                  [EXTENSION_CONTRAST_WITH]: [
+                    {
+                      color: {
+                        $type: 'color',
+                        $value: '{common.basis.color.basis.default.bg-subtle}',
+                      },
+                      ratio: 1,
+                    },
+                  ],
+                },
+                $type: 'color',
+                $value: `{ma.color.indigo.5}`,
+              },
+            },
+          },
+        },
+      },
+    };
+    const result = ThemeSchema.transform(addContrastExtensions).safeParse(config);
+    expect(result.success).toEqual(true);
+    expect(
+      result.data?.common?.basis?.color?.default?.['color-document']?.$extensions?.[EXTENSION_CONTRAST_WITH],
+    ).toHaveLength(2);
+  });
 
   describe('resolving Design Token refs', () => {
     describe('resolve color ref', () => {
