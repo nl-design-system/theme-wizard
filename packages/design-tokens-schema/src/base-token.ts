92d41783130abc84088ce20616c9a2343fcab099
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-11-14 11:00:11 +0100

    fix(design-token-schema): $deprecated, $description MUST not be null (#166)

--- packages/design-tokens-schema/src/base-token.ts
+++ packages/design-tokens-schema/src/base-token.ts
@@ -28,13 +28,13 @@ export type BaseDesignTokenIdentifier = z.infer<typeof BaseDesignTokenIdentifier
 
 export const BaseDesignTokenValueSchema = z.strictObject({
   /** @see 5.2.4 Deprecated https://www.designtokens.org/tr/drafts/format/#deprecated */
-  $deprecated: z.union([z.boolean(), z.string()]).nullish(),
+  $deprecated: z.union([z.boolean(), z.string()]).optional(),
   /** @see 5.2.1 Description https://www.designtokens.org/tr/drafts/format/#description */
-  $description: z.string().nullish(),
+  $description: z.string().optional(),
   /** @see 5.2.3 Extensions https://www.designtokens.org/tr/drafts/format/#extensions */
-  $extensions: z.record(z.string(), z.unknown()).nullish(),
+  $extensions: z.record(z.string(), z.unknown()).optional(),
   /** @see 5.2.2 Type https://www.designtokens.org/tr/drafts/format/#type-0 */
-  $type: z.string().nullish(),
+  $type: z.string().nonoptional(),
   $value: z.unknown().nonoptional(), // refine exact shape in concrete token types
 });
 export type BaseDesignTokenValue = z.infer<typeof BaseDesignTokenValueSchema>;
