88ba9816cb52a13dcfa39aca7a5e740e36e53139
Michelle <michelleermen@gmail.com>
2025-11-14 14:14:48 +0100

    chore: use issuesByPath directly

--- packages/lit/src/lib/ValidationError/index.ts
+++ packages/lit/src/lib/ValidationError/index.ts
@@ -5,13 +5,8 @@ export default class ValidationError {
   readonly issues: z.core.$ZodIssue[];
   readonly token: unknown;
 
-  constructor(path: string, errors: z.ZodError, token: unknown) {
+  constructor(path: string, errors: z.ZodError) {
     this.path = path;
-    this.token = token;
     this.issues = errors.issues;
-  }
-
-  get ok(): boolean {
-    return this.issues.length === 0;
   }
 }
