7514c1f5cf5f6e00384a9870511b9f231871e241
Michelle <michelleermen@gmail.com>
2025-11-14 13:51:12 +0100

    chore: simpler ValidationError class

--- packages/lit/src/lib/ValidationError/index.ts
+++ packages/lit/src/lib/ValidationError/index.ts
@@ -1,21 +1,14 @@
 import * as z from 'zod';
 
-export type ValidationIssue = Pick<z.core.$ZodIssue, 'code' | 'message' | 'path'>;
-
 export default class ValidationError {
   readonly path: string;
-  readonly issues: ValidationIssue[];
+  readonly issues: z.core.$ZodIssue[];
   readonly token: unknown;
 
   constructor(path: string, errors: z.ZodError, token: unknown) {
     this.path = path;
     this.token = token;
-
-    this.issues = this.#getIssues(errors);
-  }
-
-  #getIssues(errors: z.ZodError) {
-    return errors.issues;
+    this.issues = errors.issues;
   }
 
   get ok(): boolean {
