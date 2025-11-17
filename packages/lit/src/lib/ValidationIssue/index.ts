d1795b71d9bec9a15b07ee939ba33c453e0111d4
Michelle <michelleermen@gmail.com>
2025-11-17 16:55:34 +0100

    fix: typing of errorcode

--- packages/lit/src/lib/ValidationIssue/index.ts
+++ packages/lit/src/lib/ValidationIssue/index.ts
@@ -1,18 +1,19 @@
+import { ERROR_CODES } from '@nl-design-system-community/design-tokens-schema';
 import * as z from 'zod';
 
 export default class ValidationIssue {
   // TODO: move to application level if necessary
   path: string;
-  code: ErrorCode;
+  code: (typeof ERROR_CODES)[keyof typeof ERROR_CODES];
   variables: Record<string, string> = {};
   issue: z.core.$ZodIssue;
 
-  constructor(issue: z.core.$ZodIssue, code?: ErrorCode) {
+  constructor(issue: z.core.$ZodIssue, code?: string) {
     this.issue = issue;
     this.path = issue.path
       .filter((p) => p !== '$value')
       .map(String)
       .join('.');
-    this.code = code || (issue.code as ErrorCode);
+    this.code = code || (issue.code as (typeof ERROR_CODES)[keyof typeof ERROR_CODES]);
   }
 }
