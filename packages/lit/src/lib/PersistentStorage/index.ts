28a460709442c204eeef12ae9a3bbf97349c4cad
Jurgen Beliën <jurgen@voorhoede.nl>
2025-11-18 16:29:02 +0100

    feat: persist theme tokens in localstorage (#151)
    Resolves #141 
    
    * Adds reusable PersistentStorage class that defaults to localstorage
    with sessionStorage fallback
    * Supports saving/loading JSON
    * Creates namespaced keys with versioning to make migrations later
    easier

---
+++ packages/lit/src/lib/PersistentStorage/index.ts
@@ -0,0 +1,103 @@
+export const STORAGE_TYPES = {
+  LOCAL_STORAGE: 'localStorage',
+  SESSION_STORAGE: 'sessionStorage',
+} as const;
+
+type StorageType = (typeof STORAGE_TYPES)[keyof typeof STORAGE_TYPES];
+
+type Options = {
+  type?: StorageType;
+  prefix?: string;
+};
+
+const JSON_PREFIX = 'JSON';
+const JSON_SUFFIX_DEFAULT = '_';
+const SEPARATOR = ':';
+
+export default class PersistentStorage {
+  static version = 0; // Helpful for when might need to migrate persisted data
+  #type: StorageType;
+  #backend: Storage;
+  #prefix: string;
+
+  constructor({ prefix = '', type: requestedType = STORAGE_TYPES.LOCAL_STORAGE }: Options = {}) {
+    let type = requestedType;
+    const backupType = STORAGE_TYPES.SESSION_STORAGE;
+
+    if (!PersistentStorage.#isSupported(type)) {
+      if (type !== backupType) {
+        console.error(`Cannot create persistent storage, no support for ${type}.`);
+      }
+      type = backupType;
+      if (!PersistentStorage.#isSupported(backupType)) {
+        throw new Error(`Cannot create persistent storage, no support for ${backupType}.`);
+      }
+    }
+
+    this.#prefix = prefix;
+    this.#type = type;
+    this.#backend = window[type];
+  }
+
+  get length() {
+    return this.#backend.length;
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  path(...args: string[]) {
+    const v = `v${PersistentStorage.version}`;
+    return [v, this.#prefix, ...args].filter(Boolean).join(SEPARATOR);
+  }
+
+  getItem(key: string) {
+    return this.#backend.getItem(this.path(key));
+  }
+
+  getJSON(key: string = JSON_SUFFIX_DEFAULT) {
+    const value = this.getItem(this.path(JSON_PREFIX, key));
+    return value && JSON.parse(value);
+  }
+
+  removeItem(key: string) {
+    this.#backend.removeItem(this.path(key));
+  }
+
+  removeJSON(key: string = JSON_SUFFIX_DEFAULT) {
+    this.removeItem(this.path(JSON_PREFIX, key));
+  }
+
+  setItem(key: string, value: string) {
+    this.#backend.setItem(this.path(key), value);
+  }
+
+  setJSON(key: string, value: Record<string, unknown>): void;
+  setJSON(value: Record<string, unknown>): void;
+  setJSON(keyOrValue: string | Record<string, unknown>, value?: Record<string, unknown>) {
+    const key = typeof keyOrValue === 'string' ? keyOrValue : JSON_SUFFIX_DEFAULT;
+    const data = value || keyOrValue;
+    this.setItem(this.path(JSON_PREFIX, key), JSON.stringify(data));
+  }
+
+  /** @see: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API */
+  static #isSupported(type: StorageType) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === 'QuotaExceededError' &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+}
