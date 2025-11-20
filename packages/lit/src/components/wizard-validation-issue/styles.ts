2391946a41ccaf13dc05b4d10b1bea5c84cd952b
Michelle <michelleermen@gmail.com>
2025-11-20 16:57:20 +0100

    fix: lint

--- packages/lit/src/components/wizard-validation-issue/styles.ts
+++ packages/lit/src/components/wizard-validation-issue/styles.ts
@@ -11,9 +11,9 @@ export default css`
 
   .utrecht-form-field-error-message {
     border-inline-start: 2px solid var(--utrecht-form-field-error-message-color, #d52b1e);
-    padding-inline-start: 0.5rem;
     color: var(--utrecht-form-field-error-message-color, #d52b1e);
     font-size: var(--utrecht-form-field-error-message-font-size, 0.875rem);
     margin-block-start: var(--utrecht-form-field-error-message-margin-block-start, 0.25rem);
+    padding-inline-start: 0.5rem;
   }
 `;
