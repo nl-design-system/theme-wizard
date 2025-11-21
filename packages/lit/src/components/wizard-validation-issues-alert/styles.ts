513f8388b0e257dd3896b36437bdb8e4d09123e4
Michelle <michelleermen@gmail.com>
2025-11-21 13:58:08 +0100

    fix: change a tag to button

--- packages/lit/src/components/wizard-validation-issues-alert/styles.ts
+++ packages/lit/src/components/wizard-validation-issues-alert/styles.ts
@@ -1,15 +1,6 @@
 import { css } from 'lit';
+import { tokenLinkStyles } from '../wizard-token-navigator/styles';
 
 export default css`
-  .theme-token-link {
-    background: none;
-    border: none;
-    border-radius: 2px;
-    color: var(--utrecht-link-color);
-    text-decoration: underline;
-  }
-
-  .theme-token-link:focus:not(:focus-visible) {
-    outline: none;
-  }
+  ${tokenLinkStyles}
 `;
