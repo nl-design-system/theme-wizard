73c10ac7f5da5a598b4226b2ce6477207734e35a
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2025-11-21 14:35:41 +0100

    Add Playwright tests for "Download tokens as JSON" button (#188)
    - implement tests for all states
    - add missing feature where download should be disabled in case of
    validation errors
    - Separate tests into global page tests, wizard tests and template tests
    (1st commit)
    
    closes #181

---
+++ packages/theme-wizard-website/e2e/wizard.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from './fixtures/fixtures';
+
+test('can change heading font to Courier New on preview', async ({ previewPage }) => {
+  const heading = previewPage.getHeading(2);
+
+  await expect(heading).not.toHaveFont('Courier New');
+  await previewPage.changeHeadingFont('Courier New');
+  await expect(heading).toHaveFont('Courier New');
+});
+
+test('can change body font to Arial', async ({ previewPage }) => {
+  const paragraph = previewPage.getParagraph();
+
+  await expect(paragraph).not.toHaveFont('Arial');
+  await previewPage.changeBodyFont('Arial');
+  await expect(paragraph).toHaveFont('Arial');
+});
+
+test.describe('Download tokens as JSON', () => {
+  test('initial button state is correct', async ({ themeWizard }) => {
+    await expect(themeWizard.downloadButton).toBeVisible();
+    await expect(themeWizard.downloadButton).toBeDisabled();
+  });
+
+  test.describe('after changing a token', () => {
+    test.beforeEach(async ({ themeWizard }) => {
+      await themeWizard.changeBodyFont('Arial');
+    });
+
+    test('Button becomes active after changes made', async ({ themeWizard }) => {
+      await expect(themeWizard.downloadButton).toBeEnabled();
+    });
+
+    test('Button downloads JSON file after click', async ({ page, themeWizard }) => {
+      const downloadPromise = page.waitForEvent('download');
+      await themeWizard.downloadButton.click();
+      const download = await downloadPromise;
+      expect(download.suggestedFilename()).toBe('tokens.json');
+    });
+
+    test('Button becomes inactive after "Reset tokens" is clicked', async ({ themeWizard }) => {
+      await themeWizard.reset();
+      await expect(themeWizard.downloadButton).toBeDisabled();
+    });
+
+    test('Button is disabled when validation errors are found', async ({ themeWizard }) => {
+      // Make sure the color inputs are visible so we can interact with them
+      await themeWizard.sidebar.locator('summary').click();
+      // Trigger a contrast warning
+      await themeWizard.changeColor('{basis.color.accent-1.bg-active}', '#000000');
+
+      await expect(themeWizard.downloadButton).toBeDisabled();
+    });
+  });
+});
