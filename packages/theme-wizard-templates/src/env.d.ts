145fe11f9cc6ab36b551220e0a895cbc0daa944b
Michelle <michelleermen@gmail.com>
2025-12-04 13:35:05 +0100

    awful wip

--- packages/theme-wizard-templates/src/env.d.ts
+++ packages/theme-wizard-templates/src/env.d.ts
@@ -1,1 +1,4 @@
 declare const __STANDALONE_PACKAGE__: boolean;
+
+declare module '@nl-design-system-community/theme-wizard-app';
+declare module '@utrecht/web-component-library-stencil/loader/index.js';
