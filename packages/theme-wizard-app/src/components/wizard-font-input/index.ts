2aeab1443d72ea7c67bf117ba36161cce321e93b
Jurgen Beliën <jurgen@voorhoede.nl>
2025-12-04 16:14:13 +0100

    feat(wizard-font-input): use combobox

--- packages/theme-wizard-app/src/components/wizard-font-input/index.ts
+++ packages/theme-wizard-app/src/components/wizard-font-input/index.ts
@@ -1,5 +1,7 @@
+import '@nl-design-system-community/clippy-components/clippy-combobox';
+import { ClippyCombobox } from '@nl-design-system-community/clippy-components/clippy-combobox';
 import { ModernFontFamilyToken } from '@nl-design-system-community/design-tokens-schema';
-import { html, nothing } from 'lit';
+import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { t } from '../../i18n';
 import { WizardTokenInput } from '../wizard-token-input';
@@ -27,7 +29,7 @@ declare global {
 export class WizardFontInput extends WizardTokenInput {
   @property() defaultOptionsLabel = 'Standaardopties';
   @property() optionsLabel = 'Opties';
-  @property() options: FontOption[] = [];
+  @property({ type: Array }) options: FontOption[] = [];
   readonly #token: ModernFontFamilyToken = {
     $type: 'fontFamily',
     $value: '',
@@ -42,74 +44,34 @@ export class WizardFontInput extends WizardTokenInput {
     const oldValue = this.#token.$value;
     this.#token.$value = value;
     this.internals_.setFormValue(WizardFontInput.valueAsString(value));
-
-    const optionIsValue = this.valueComparator(value);
-    if (!DEFAULT_FONT_OPTIONS.some(optionIsValue) && !this.options.some(optionIsValue)) {
-      const label = (Array.isArray(value) ? value[0] : value).replaceAll(/['"]/g, '');
-      this.options.push({ label, value });
-    }
     this.requestUpdate('value', oldValue);
   }
 
   readonly #handleChange = (event: Event) => {
-    if (event.target instanceof HTMLSelectElement) {
-      try {
-        this.value = JSON.parse(event.target.value);
-        this.dispatchEvent(new Event('change', { bubbles: true, composed: true }));
-      } catch {
-        // reset to previous version when parsing fails
-        event.target.value = WizardTokenInput.valueAsString(this.value);
-      }
-    }
+    const target = event.target;
+    if (!(target instanceof ClippyCombobox)) return;
+    this.value = `${target.value}`;
+    this.dispatchEvent(new Event('change', { bubbles: true, composed: true }));
   };
-
-  valueComparator(value: FontOption['value'] = this.value) {
-    // Simple conversion to string ought to be enough to compare
-    return (option: FontOption) => `${option.value}` === `${value}`;
-  }
 
   override render() {
     return html`
-      <label for=${this.id}>${this.label}</label>
-      ${this.errors.map(
-        (error) =>
-          html`<div class="utrecht-form-field-error-message" id=${error.id}>
-            ${t(`validation.error.${error.code}.compact`, error)}
-          </div>`,
-      )}
-      <select
-        id=${this.id}
-        name=${this.name}
-        class=${this.hasErrors ? 'theme-error' : ''}
-        @change=${this.#handleChange}
-        aria-invalid=${this.hasErrors ? 'true' : nothing}
-        aria-errormessage=${this.hasErrors ? this.errors.map((error) => error.id).join(' ') : nothing}
-      >
-        ${this.options.length
-          ? html`<optgroup label=${this.optionsLabel}>
-              ${this.options.map(
-                (option) =>
-                  html`<option
-                    value=${WizardTokenInput.valueAsString(option.value)}
-                    ?selected=${this.valueComparator(this.value)(option)}
-                  >
-                    ${option.label}
-                  </option>`,
-              )}
-            </optgroup>`
-          : nothing}
-        <optgroup label=${this.defaultOptionsLabel}>
-          ${DEFAULT_FONT_OPTIONS.map(
-            (option) =>
-              html`<option
-                value=${WizardTokenInput.valueAsString(option.value)}
-                ?selected=${this.valueComparator(this.value)(option)}
-              >
-                ${option.label}
-              </option>`,
-          )}
-        </optgroup>
-      </select>
+      <div class="utrecht-form-field__input">
+        <label id="label-${this.name}">${this.label}</label>
+        ${this.errors.map(
+          (error) =>
+            html`<div class="utrecht-form-field-error-message" id=${error.id}>
+              ${t(`validation.error.${error.code}.compact`, error)}
+            </div>`,
+        )}
+        <clippy-combobox
+          aria-labelledby="label-${this.name}"
+          name=${this.name}
+          @change=${this.#handleChange}
+          .value=${this.value.toString()}
+          .options=${DEFAULT_FONT_OPTIONS}
+        ></clippy-combobox>
+      </div>
     `;
   }
 }
