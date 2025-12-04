08c94f8866e1b2babd21de51c7a86308a13f3e46
Michelle <michelleermen@gmail.com>
2025-12-04 14:21:06 +0100

    feat: centralize theme

--- packages/theme-wizard-app/src/components/template-skip-link/index.ts
+++ packages/theme-wizard-app/src/components/template-skip-link/index.ts
@@ -1,11 +1,10 @@
 import skipLinkCss from '@nl-design-system-candidate/skip-link-css/skip-link.css?inline';
 import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import styles from './styles';
 
 @customElement('template-skip-link')
 export class TemplateSkipLink extends LitElement {
-  static override readonly styles = [unsafeCSS(skipLinkCss), styles];
+  static override readonly styles = [unsafeCSS(skipLinkCss)];
 
   override render() {
     return html` <a href="#main" class="nl-skip-link nl-skip-link--visible-on-focus">
