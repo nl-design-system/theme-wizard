875f190e0c69d6614a1218523e9fc3ca8c4b2136
Michelle <michelleermen@gmail.com>
2025-12-04 14:21:06 +0100

    feat: centralize theme

--- packages/theme-wizard-app/src/components/template-skip-link/styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'lit';
-
-export default css`
-  /* Override position to absolute to move the position to in the preview */
-  /* stylelint-disable-next-line selector-class-pattern */
-  .nl-skip-link--visible-on-focus:focus {
-    position: absolute;
-  }
-`;
