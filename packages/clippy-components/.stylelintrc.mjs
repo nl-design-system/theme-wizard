46c1c1748abe8ec93b32de719d974847e46685eb
Jurgen Beliën <jurgen@voorhoede.nl>
2025-12-08 14:15:06 +0100

    feat(clippy-components): css prefix rules

--- packages/clippy-components/.stylelintrc.mjs
+++ packages/clippy-components/.stylelintrc.mjs
@@ -1,6 +1,13 @@
 export default {
   extends: ['../../.stylelintrc.json'],
   ignoreFiles: ['**/*', '!**/*.ts'],
+  rules: {
+    'custom-property-pattern': '^_?(ams|basis|clippy|denhaag|utrecht|todo)-[a-z0-9-]+$',
+    'keyframes-name-pattern': '^(ams|basis|clippy|utrecht|todo)-[a-z0-9-]+$',
+    'scss/dollar-variable-pattern': '^(ams|basis|clippy|utrecht|todo)-[a-z0-9-]+$',
+    'scss/percent-placeholder-pattern': '^(ams|basis|clippy|utrecht|todo)-[a-z0-9-]+$',
+    'selector-class-pattern': '^(ams|basis|clippy|denhaag|utrecht|todo|wizard)-[a-z0-9_-]+$',
+  },
   overrides: [
     {
       customSyntax: 'postcss-lit',
