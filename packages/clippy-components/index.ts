3ed9206d254acbf8bff9230a02aa00843a85d9cc
Michelle <michelleermen@gmail.com>
2025-12-10 16:35:14 +0100

    fix: display name of component name

--- packages/clippy-components/index.ts
+++ packages/clippy-components/index.ts
@@ -1,6 +1,9 @@
 import './src/clippy-modal';
+import { CookieConsentDrawer as _CookieConsentDrawer } from './patterns/cookie-consent/react/CookieConsentDrawer';
 
-export { CookieConsentDrawer } from './patterns/cookie-consent/react/CookieConsentDrawer';
+// Ensure displayName is preserved after export
+export const CookieConsentDrawer = _CookieConsentDrawer;
+
 export type { CookieConsentDrawerProps } from './patterns/cookie-consent/react/CookieConsentDrawer';
 export {
   getConsentTitle,
