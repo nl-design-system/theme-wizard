5a917dd95ca28d229b0a55be45ce34db8e531858
Michelle <michelleermen@gmail.com>
2025-12-10 10:00:14 +0100

    feat: add cookie consent pattern exports with index.ts

---
+++ packages/clippy-components/patterns/cookie-consent/index.ts
@@ -0,0 +1,15 @@
+import cookieDrawer from './cookie-drawer.html?raw';
+import cookieForm from './cookie-form.html?raw';
+import cookieModal from './cookie-modal.html?raw';
+import cookiesPage from './cookies-page.html?raw';
+import readme from './README.md?raw';
+
+export { cookieDrawer, cookieForm, cookieModal, cookiesPage, readme };
+
+export default {
+  cookieDrawer,
+  cookieForm,
+  cookieModal,
+  cookiesPage,
+  readme,
+};
