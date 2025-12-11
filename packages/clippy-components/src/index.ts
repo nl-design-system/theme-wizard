c1e31f2ccd42aa65cc209b4a66f5a2d9555edd04
Michelle <michelleermen@gmail.com>
2025-12-11 14:13:21 +0100

    feat: move drawer back to storybook package

--- packages/clippy-components/src/index.ts
+++ packages/clippy-components/src/index.ts
@@ -2,10 +2,10 @@ import './clippy-combobox';
 import './clippy-font-combobox';
 import './clippy-html-image';
 import './clippy-modal';
-export { CookieConsentDrawer } from './patterns/cookie-consent/Drawer';
-export type { CookieConsentDrawerProps } from './patterns/cookie-consent/Drawer/types';
+export { CookieConsentDrawer } from '../../clippy-storybook/src/patterns/cookie-consent/Drawer';
+export type { CookieConsentDrawerProps } from '../../clippy-storybook/src/patterns/cookie-consent/Drawer/types';
 export {
   useCookieConsent,
   type UseCookieConsentOptions,
   type UseCookieConsentReturn,
-} from './patterns/cookie-consent/hooks/useCookieConsent';
+} from '../../clippy-storybook/src/patterns/cookie-consent/hooks/useCookieConsent';
