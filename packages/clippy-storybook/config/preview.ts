86da7b8e9d7c7eb5afa92cdf3231c48691e77efa
Michelle <michelleermen@gmail.com>
2025-12-11 14:51:30 +0100

    feat: move theme.css to theme-wizard-app

--- packages/clippy-storybook/config/preview.ts
+++ packages/clippy-storybook/config/preview.ts
@@ -2,7 +2,7 @@ import type { Preview } from '@storybook/web-components-vite';
 // import { StoryRootDecorator } from '@nl-design-system-candidate/storybook-shared/src/StoryRootDecorator';
 import { formatHtml } from '@rijkshuisstijl-community/storybook-tooling/formatHtml';
 // import { DocsPage } from '../src/DocsPage';
-import '@nl-design-system-community/theme-wizard-templates/theme.css';
+import '@nl-design-system-community/theme-wizard-app/theme.css';
 import '@nl-design-system-community/ma-design-tokens/dist/theme.css';
 import '@utrecht/component-library-css';
 
