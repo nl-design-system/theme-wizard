c2c1a9d4117bf61b9cd7dbf705667281ea4895cc
Jurgen Beliën <jurgen@voorhoede.nl>
2025-12-15 15:27:26 +0100

    feat(clippy-color-combobox): add nl-color-sample styles

--- packages/clippy-components/src/clippy-color-combobox/index.ts
+++ packages/clippy-components/src/clippy-color-combobox/index.ts
@@ -3,10 +3,11 @@ import { html, unsafeCSS } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 import { ClippyCombobox } from '../clippy-combobox';
+import colorComboboxStyles from './styles';
 
 type Option = {
   label: string;
-  value: string | Array<string>;
+  value: string;
 };
 
 const tag = 'clippy-color-combobox';
@@ -19,7 +20,7 @@ declare global {
 
 @customElement(tag)
 export class ClippyColorCombobox extends ClippyCombobox<Option> {
-  static override readonly styles = [...ClippyCombobox.styles, unsafeCSS(colorSampleStyles)];
+  static override readonly styles = [...ClippyCombobox.styles, colorComboboxStyles, unsafeCSS(colorSampleStyles)];
 
   override readonly filter = (option: Option) => {
     const label = `${option.label}`; // Use as string
@@ -31,9 +32,9 @@ export class ClippyColorCombobox extends ClippyCombobox<Option> {
   }
 
   override renderEntry(option: Option) {
-    const color = 'red';
+    const color = option.value;
     return html`
-      <span>
+      <span class="clippy-color-combobox__option">
         <svg role="img" xmlns="http://www.w3.org/2000/svg" class="nl-color-sample" style=${styleMap({ color })}>
           <path d="M0 0H32V32H0Z" fill="currentcolor" />
         </svg>
