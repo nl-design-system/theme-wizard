46bac0fe11edd64418e87e9cc5ef90406894c2c1
Jurgen Beliën <jurgen@voorhoede.nl>
2025-12-15 10:40:58 +0100

    style(e2e): indentation in fixtures

--- packages/theme-wizard-website/e2e/fixtures/fixtures.ts
+++ packages/theme-wizard-website/e2e/fixtures/fixtures.ts
@@ -19,51 +19,51 @@ type MatcherResult =
 
 export const expect = baseExpect.extend({
   async toHaveFont(locator: Locator, expectedFont: string, options?: { timeout?: number }) {
-      const assertionName = 'toHaveFont';
-      let pass: boolean;
-      let matcherResult: MatcherResult = undefined;
+    const assertionName = 'toHaveFont';
+    let pass: boolean;
+    let matcherResult: MatcherResult = undefined;
 
-      try {
-        const expectation = this.isNot ? baseExpect(locator).not : baseExpect(locator);
-        await expectation.toHaveCSS('font-family', new RegExp(expectedFont), options);
-        pass = true;
-      } catch (error: unknown) {
-        if (error instanceof Error && 'matcherResult' in error) {
-          matcherResult = error.matcherResult as MatcherResult;
-        } else {
-          throw new Error('Failed to get font family');
-        }
-        pass = false;
+    try {
+      const expectation = this.isNot ? baseExpect(locator).not : baseExpect(locator);
+      await expectation.toHaveCSS('font-family', new RegExp(expectedFont), options);
+      pass = true;
+    } catch (error: unknown) {
+      if (error instanceof Error && 'matcherResult' in error) {
+        matcherResult = error.matcherResult as MatcherResult;
+      } else {
+        throw new Error('Failed to get font family');
       }
+      pass = false;
+    }
 
-      if (this.isNot) {
-        pass = !pass;
-      }
+    if (this.isNot) {
+      pass = !pass;
+    }
 
-      const locatorString = (locator as { toString(): string }).toString();
+    const locatorString = (locator as { toString(): string }).toString();
 
-      const message = pass
-        ? () =>
-            this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
-            '\n\n' +
-            `Locator: ${locatorString}\n` +
-            `Expected: not ${this.utils.printExpected(expectedFont)}\n` +
-            (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '')
-        : () =>
-            this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
-            '\n\n' +
-            `Locator: ${locatorString}\n` +
-            `Expected: ${this.utils.printExpected(expectedFont)}\n` +
-            (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '');
+    const message = pass
+      ? () =>
+          this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
+          '\n\n' +
+          `Locator: ${locatorString}\n` +
+          `Expected: not ${this.utils.printExpected(expectedFont)}\n` +
+          (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '')
+      : () =>
+          this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
+          '\n\n' +
+          `Locator: ${locatorString}\n` +
+          `Expected: ${this.utils.printExpected(expectedFont)}\n` +
+          (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '');
 
-      return {
-        name: assertionName,
-        actual: matcherResult?.actual,
-        expected: expectedFont,
-        message,
-        pass,
-      };
-    },
+    return {
+      name: assertionName,
+      actual: matcherResult?.actual,
+      expected: expectedFont,
+      message,
+      pass,
+    };
+  },
 });
 
 export const test = baseTest.extend<ThemeWizardFixture>({
