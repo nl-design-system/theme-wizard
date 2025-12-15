fef6241c77a99320f3cc73ef8b428afc8d6b810e
Michelle <michelleermen@gmail.com>
2025-12-15 15:51:03 +0100

    fix: rollup

--- packages/theme-wizard-app/vite.config.ts
+++ packages/theme-wizard-app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['lit'],
+      external: ['lit', 'lit/directives/class-map.js', 'lit/directives/unsafe-svg.js'],
     },
   },
 });
