ab559866132b8a272e1d3bef4f6152784cf3dc97
Bart Veneman <bartveneman@gmail.com>
2025-12-17 10:58:24 +0100

    restore external fonts

--- packages/clippy-components/src/clippy-font-combobox/external.ts
+++ packages/clippy-components/src/clippy-font-combobox/external.ts
@@ -1,9 +1,7 @@
-// import { items } from './google-fonts.json';
+import { items } from './google-fonts.json';
 
-// export default items.map(({ category, family }) => ({
-//   cssUrl: `https://fonts.googleapis.com/css2?family=${encodeURIComponent(family)}&display=swap`,
-//   label: family,
-//   value: [family, ...['serif', 'sans-serif', 'monospace'].filter((type) => type === category)],
-// }));
-
-export default [];
+export default items.map(({ category, family }) => ({
+  cssUrl: `https://fonts.googleapis.com/css2?family=${encodeURIComponent(family)}&display=swap`,
+  label: family,
+  value: [family, ...['serif', 'sans-serif', 'monospace'].filter((type) => type === category)],
+}));
