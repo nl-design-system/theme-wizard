b314b34f10cf6e8e5af79c16e3a9d1ad3b77e165
Bart Veneman <bartveneman@gmail.com>
2025-12-22 16:34:39 +0100

    lint

--- packages/theme-wizard-app/src/components/wizard-style-guide/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-style-guide/styles.ts
@@ -5,13 +5,6 @@ export default css`
     display: grid;
     padding-block: var(--basis-space-block-xl);
     padding-inline: var(--basis-space-inline-xl);
-<<<<<<< HEAD
-||||||| parent of e8a0f8a (prettier side nav)
-    display: grid;
-    row-gap: var(--basis-space-block-3xl);
-=======
-    display: grid;
->>>>>>> e8a0f8a (prettier side nav)
     row-gap: var(--basis-space-row-5xl);
   }
 
@@ -27,7 +20,6 @@ export default css`
   }
 
   .wizard-styleguide__nav-item {
-<<<<<<< HEAD
     border-inline-start: 4px solid transparent;
     color: var(--basis-color-accent-1-color-default);
     display: block;
@@ -36,17 +28,13 @@ export default css`
     padding-block: var(--basis-space-block-xl);
     padding-inline: var(--basis-space-inline-2xl);
     text-decoration: none;
-||||||| parent of e8a0f8a (prettier side nav)
-    row-gap: var(--basis-space-block-2xl);
-=======
-    display: block;
     border-inline-start: 4px solid transparent;
-    padding-inline: var(--basis-space-inline-2xl);
-    padding-block: var(--basis-space-block-xl);
-    text-decoration: none;
-    font-weight: var(--basis-text-font-weight-bold);
+    display: block;
     font-size: var(--basis-text-font-size-lg);
->>>>>>> e8a0f8a (prettier side nav)
+    font-weight: var(--basis-text-font-weight-bold);
+    padding-block: var(--basis-space-block-xl);
+    padding-inline: var(--basis-space-inline-2xl);
+    text-decoration: none;
 
     &:hover,
     &:focus-visible {
