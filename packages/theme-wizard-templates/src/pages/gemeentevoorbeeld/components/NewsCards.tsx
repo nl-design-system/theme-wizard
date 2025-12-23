62b23f2c86d8c027e993b9fd313eeb911a7a95c0
Michelle <michelleermen@gmail.com>
2025-12-23 15:35:45 +0100

    feat: replace chevron icon with read more button for consistency

--- packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/NewsCards.tsx
+++ packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/NewsCards.tsx
@@ -1,9 +1,8 @@
 import { Card } from '@amsterdam/design-system-react';
 import { Heading } from '@nl-design-system-candidate/heading-react/css';
-import { Link } from '@nl-design-system-candidate/link-react';
 import { Icon } from '@utrecht/component-library-react';
-import { Paragraph } from '@utrecht/component-library-react/dist/css-module';
-import { UtrechtIconChevronRight, UtrechtIconKalender } from '@utrecht/web-component-library-react';
+import { Paragraph, ButtonLink } from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconKalender } from '@utrecht/web-component-library-react';
 import React, { type FC } from 'react';
 import type { NewsItem } from '../types';
 import { Column, Row } from './Layout';
@@ -16,7 +15,7 @@ const NewsCards: FC<NewsCardsProps> = ({ cards }) => (
   <Row columnGap="var(--basis-space-column-xl)" justify="flex-start" rowGap="var(--basis-space-row-xl)">
     {cards.map((card) => (
       <Column key={`${card.href}-${card.title}`} cols={6}>
-        <Card className="voorbeeld__link voorbeeld__link--news">
+        <Card aria-label={`Lees meer over ${card.title}`} className=" voorbeeld__link">
           <Heading level={3} appearance="level-3">
             {card.title}
           </Heading>
@@ -30,12 +29,9 @@ const NewsCards: FC<NewsCardsProps> = ({ cards }) => (
           <Paragraph>{card.body}</Paragraph>
 
           <Row justify="flex-end">
-            {/* https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA8 */}
-            <Link href={card.href} aria-label={`Lees meer over ${card.title}`}>
-              <Icon aria-hidden="true">
-                <UtrechtIconChevronRight />
-              </Icon>
-            </Link>
+            <ButtonLink appearance="secondary-action-button" href="/">
+              Meer lezen
+            </ButtonLink>
           </Row>
         </Card>
       </Column>
