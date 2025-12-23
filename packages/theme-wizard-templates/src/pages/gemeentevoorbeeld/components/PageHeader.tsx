b78219d21c931b13cf9240dc5ab7db835b7c6601
Michelle <michelleermen@gmail.com>
2025-12-23 12:01:01 +0100

    feat: make PageHeaderSection configurable

--- packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/PageHeader.tsx
+++ packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/PageHeader.tsx
@@ -1,34 +1,45 @@
-import { Link, PageContent } from '@utrecht/component-library-react/dist/css-module';
-import React, { type FC } from 'react';
+import { Icon, Link, PageContent } from '@utrecht/component-library-react/dist/css-module';
+import { UtrechtIconAlleen } from '@utrecht/web-component-library-react';
+import React, { type FC, type ReactNode } from 'react';
 import logo from '../../../assets/logo.svg';
 import { Column, Row } from './Layout';
 
 export interface PageHeaderProps {
-  actions?: Array<{ href: string; label: string }>;
+  actions?: Array<{ href: string; label: string; icon?: string }>;
+  children?: ReactNode;
 }
 
 const DEFAULT_ACTIONS = [
   { href: '#', label: 'Contact' },
-  { href: '#', label: 'Mijn Omgeving' },
+  { href: '#', icon: 'utrecht-icon-alleen', label: 'Mijn Omgeving' },
 ] as const;
 
-const PageHeaderSection: FC<PageHeaderProps> = ({ actions = DEFAULT_ACTIONS }) => (
+const PageHeaderSection: FC<PageHeaderProps> = ({ actions = DEFAULT_ACTIONS, children }) => (
   <PageContent>
     <div className="section">
       <Row align="center" justify="space-between">
-        <Column cols={6}>
-          <Link href="/" aria-label="home / logo">
-            <img src={typeof logo === 'string' ? logo : logo.src} alt="Gemeente Voorbeeld" />
-          </Link>
-        </Column>
+        {children ?? (
+          <>
+            <Column cols={6}>
+              <Link href="/" aria-label="home / logo">
+                <img src={typeof logo === 'string' ? logo : logo.src} alt="Gemeente Voorbeeld" />
+              </Link>
+            </Column>
 
-        <Column cols={6} className="page-header__actions">
-          {actions.map(({ href, label }) => (
-            <Link key={`${href}-${label}`} href={href}>
-              {label}
-            </Link>
-          ))}
-        </Column>
+            <Column cols={6} className="page-header__actions">
+              {actions.map(({ href, icon, label }) => (
+                <Link key={`${href}-${label}`} className="page-header__action-link" href={href}>
+                  {icon && (
+                    <Icon>
+                      <UtrechtIconAlleen />
+                    </Icon>
+                  )}
+                  {label}
+                </Link>
+              ))}
+            </Column>
+          </>
+        )}
       </Row>
     </div>
   </PageContent>
