7518fb324560a314c48f89f20eeeca48c088c439
Michelle <michelleermen@gmail.com>
2026-01-05 16:51:24 +0100

    fix(storybook): enable react/jsx-uses-react to mark React as used with JSX

--- packages/clippy-storybook/eslint.config.mjs
+++ packages/clippy-storybook/eslint.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'eslint/config';
 import rootConfig from '../../eslint.config.mjs';
 
-export default defineConfig([...rootConfig]);
+export default defineConfig([
+  ...rootConfig,
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      'react/jsx-uses-react': 'error',
+    },
+  },
+]);
