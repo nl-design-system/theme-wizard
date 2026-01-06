bc335ccbed137f9ccb54d7b531f83674a9a2ed57
Michelle <michelleermen@gmail.com>
2026-01-06 13:52:18 +0100

    chore: add decorator function for adding theme classes to story

--- packages/clippy-storybook/src/GemeenteVoorbeeldHome.stories.tsx
+++ packages/clippy-storybook/src/GemeenteVoorbeeldHome.stories.tsx
@@ -3,25 +3,11 @@ import GemeenteVoorbeeldHome, {
   type GemeenteVoorbeeldHomeProps,
 } from '@nl-design-system-community/theme-wizard-templates/src/pages/gemeentevoorbeeld/GemeenteVoorbeeldHome';
 import * as React from 'react';
+import '@utrecht/component-library-css';
 import documentation from '../docs/templates/gemeente-voorbeeld-documentatie.md?raw';
 
 const meta = {
   component: GemeenteVoorbeeldHome as React.ComponentType<GemeenteVoorbeeldHomeProps>,
-  decorators: [
-    (Story) => {
-      if (typeof globalThis !== 'undefined' && 'document' in globalThis) {
-        const doc = (globalThis as typeof globalThis & { document?: Document }).document;
-        const html = doc?.documentElement;
-        const body = doc?.body;
-
-        html?.classList.add('utrecht-page-layout', 'utrecht-root', 'ma-theme');
-        html?.classList.remove('start-theme');
-        body?.classList.add('utrecht-page-body');
-      }
-
-      return <Story />;
-    },
-  ],
   parameters: {
     docs: {
       description: {
@@ -38,5 +24,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: 'Home',
+  name: 'Gemeente Voorbeeld',
 };
