d76e4d14aea61e689ffac486d7ea821038b43057
Michelle <michelleermen@gmail.com>
2026-01-08 16:38:39 +0100

    chore: svg support

--- packages/theme-wizard-templates/src/env.d.ts
+++ packages/theme-wizard-templates/src/env.d.ts
@@ -2,3 +2,12 @@ declare const __STANDALONE_PACKAGE__: boolean;
 
 declare module '@nl-design-system-community/theme-wizard-app';
 declare module '@utrecht/web-component-library-stencil/loader/index.js';
+
+declare module '*.svg' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
+}
