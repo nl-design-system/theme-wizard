776082549fa5584265fd6561a11b4441855634d5
Jurgen Beliën <jurgen@voorhoede.nl>
2026-01-12 11:29:52 +0100

    test(playwright): rewrite fallback to defaults

--- packages/theme-wizard-website/playwright.config.ts
+++ packages/theme-wizard-website/playwright.config.ts
@@ -10,14 +10,14 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: Number(process.env.E2E_TIMEOUT_EXPECT || '5000'),
+    timeout: Number(process.env['E2E_TIMEOUT_EXPECT'] || 5000),
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: Boolean(process.env.CI),
+  forbidOnly: Boolean(process.env['CI']),
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Maximum time the entire test suite can run for */
-  globalTimeout: Number(process.env.E2E_TIMEOUT_GLOBAL || '120_000'),
+  globalTimeout: Number(process.env['E2E_TIMEOUT_GLOBAL'] || 120_000),
   outputDir: './tmp/playwright-results/',
   /* Configure projects for major browsers */
   projects: [
@@ -32,19 +32,18 @@ const config: PlaywrightTestConfig = {
     },
   ],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['github'], ['list']] : 'list',
-  retries: Number(process.env.E2E_RETRIES || '1'),
+  reporter: process.env['CI'] ? [['github'], ['list']] : 'list',
+  retries: Number(process.env['E2E_RETRIES'] || 1),
   testDir: './e2e',
   testMatch: '**/*spec.ts',
   /* Maximum time one test can run for. */
-  timeout: Number(process.env.E2E_TIMEOUT_TEST || '7500'),
+  timeout: Number(process.env['E2E_TIMEOUT_TEST'] || 7500),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.E2E_TARGET_URL || 'http://localhost:9492',
-
+    baseURL: process.env['E2E_TARGET_URL'] || 'http://localhost:9492',
     screenshot: {
       fullPage: true,
       mode: 'on-first-failure',
@@ -60,7 +59,7 @@ const config: PlaywrightTestConfig = {
     },
   },
 
-  ...(process.env.E2E_TARGET_URL && !process.env.E2E_TARGET_URL?.startsWith('http://localhost'))
+  ...(process.env['E2E_TARGET_URL'] && !process.env['E2E_TARGET_URL']?.startsWith('http://localhost'))
     ? {}
     : { webServer: [
         {
@@ -68,7 +67,7 @@ const config: PlaywrightTestConfig = {
           command: 'pnpm run dev',
           cwd: '../theme-wizard-server',
           port: 9491,
-          reuseExistingServer: !process.env.CI,
+          reuseExistingServer: !process.env['CI'],
           // Log server errors directly to the main output for easier debugging in CI
           stderr: 'pipe',
           // How long the server can take to start up
@@ -78,7 +77,7 @@ const config: PlaywrightTestConfig = {
           name: 'Website',
           command: 'pnpm run dev',
           port: 9492,
-          reuseExistingServer: !process.env.CI,
+          reuseExistingServer: !process.env['CI'],
           // How long the server can take to start up
           timeout: 10_000,
         },
