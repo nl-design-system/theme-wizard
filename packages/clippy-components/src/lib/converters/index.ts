963de35d187bf6335c3fa7b943657ad253f0d475
Jurgen Beliën <jurgen@voorhoede.nl>
2026-01-20 14:30:49 +0100

    feat(converters): improve space-separated attribute converter

--- packages/clippy-components/src/lib/converters/index.ts
+++ packages/clippy-components/src/lib/converters/index.ts
@@ -2,12 +2,14 @@ import type { ComplexAttributeConverter } from 'lit';
 
 type AttributeConverterFrom<T> = NonNullable<ComplexAttributeConverter<T>['fromAttribute']>;
 
-export const arrayFromTokenList: AttributeConverterFrom<Array<unknown> | null> = (value) => {
+const createArrayConverter = (delimiter: string | RegExp): AttributeConverterFrom<Array<unknown> | null> => (value) => {
   if (value === null) return value;
   try {
     const json = value ? JSON.parse(value) : value;
     return Array.isArray(json) ? json : null;
   } catch {
-    return value.split(' ');
+    return value.trim().split(delimiter);
   }
 };
+
+export const arrayFromTokenList: AttributeConverterFrom<Array<unknown> | null> = createArrayConverter(/\s+/);
