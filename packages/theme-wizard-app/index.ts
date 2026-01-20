bc8cc6bbd79d08bbf9289cdc8861de69a6842e76
Michelle <michelleermen@gmail.com>
2026-01-20 11:57:48 +0100

    feat: add export of template components for templates

--- packages/theme-wizard-app/index.ts
+++ packages/theme-wizard-app/index.ts
@@ -1,1 +1,2 @@
 import './src/components/app/app';
+import './src/template-components';
---
+++ packages/theme-wizard-app/src/template-components.ts
@@ -0,0 +1,8 @@
+import './components/template-skip-link';
+import './components/template-page-header';
+import './components/template-link';
+import './components/template-link-list';
+import './components/template-heading';
+import './components/template-side-nav';
+import './components/template-paragraph';
+
