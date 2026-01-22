30e3dd4d6618d55764c74fa1b95cc3a0c5849c80
Michelle <83402290+ermenm@users.noreply.github.com>
2026-01-22 15:13:56 +0100

    GVB Template Docs & Theme Switcher Storybook (#425)
    - Theme switcher in storybook for some municipalities, start-theme,
    ma-theme and voorbeeld-theme
    - docs page for using GVB template
    - cleaned up unused CSS
    - Moved --clippy variables to separate file and import this file for the
    .preview-theme file

---
+++ packages/clippy-storybook/config/themes/theme-data.ts
@@ -0,0 +1,173 @@
+export type ThemeConfig = {
+  className: string;
+  title: string;
+  href: string;
+  package: string;
+};
+
+export const THEMES: ThemeConfig[] = [
+  {
+    className: 'voorbeeld-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/voorbeeld-design-tokens/dist/theme.css',
+    package: '@nl-design-system-unstable/voorbeeld-design-tokens',
+    title: 'Voorbeeld thema',
+  },
+  {
+    className: 'start-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/start-design-tokens/dist/theme.css',
+    package: '@nl-design-system-unstable/start-design-tokens',
+    title: 'Start thema',
+  },
+  {
+    className: 'ma-theme',
+    href: 'https://unpkg.com/@nl-design-system-community/ma-design-tokens/dist/theme.css',
+    package: '@nl-design-system-unstable/start-design-tokens',
+    title: 'Mooi & Anders thema',
+  },
+  {
+    className: 'amsterdam-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/amsterdam-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/amsterdam-design-tokens',
+    title: 'Gemeente Amsterdam',
+  },
+  {
+    className: 'bodegravenreeuwijk-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/bodegraven-reeuwijk-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/bodegraven-reeuwijk-design-tokens',
+    title: 'Gemeente Bodegraven-Reeuwijk',
+  },
+  {
+    className: 'buren-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/buren-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/buren-design-tokens',
+    title: 'Gemeente Buren',
+  },
+  {
+    className: 'denhaag-theme',
+    href: 'https://unpkg.com/@gemeente-denhaag/design-tokens-components/dist/theme/index.css',
+    package: '@gemeente-denhaag/design-tokens-components',
+    title: 'Gemeente Den Haag',
+  },
+  {
+    className: 'drechterland-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/drechterland-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/drechterland-design-tokens',
+    title: 'Gemeente Drechterland',
+  },
+  {
+    className: 'duiven-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/duiven-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/duiven-design-tokens',
+    title: 'Gemeente Duiven',
+  },
+  {
+    className: 'enkhuizen-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/enkhuizen-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/enkhuizen-design-tokens',
+    title: 'Gemeente Enkhuizen',
+  },
+  {
+    className: 'groningen-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/groningen-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/groningen-design-tokens',
+    title: 'Gemeente Groningen',
+  },
+  {
+    className: 'haarlem-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/haarlem-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/haarlem-design-tokens',
+    title: 'Gemeente Haarlem',
+  },
+  {
+    className: 'haarlemmermeer-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/haarlemmermeer-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/haarlemmermeer-design-tokens',
+    title: 'Gemeente Haarlemmermeer',
+  },
+  {
+    className: 'hoorn-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/hoorn-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/hoorn-design-tokens',
+    title: 'Gemeente Hoorn',
+  },
+  {
+    className: 'horstaandemaas-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/horstaandemaas-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/horstaandemaas-design-tokens',
+    title: 'Gemeente Horst aan de Maas',
+  },
+  {
+    className: 'leidschendamvoorburg-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/leidschendam-voorburg-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/leidschendam-voorburg-design-tokens',
+    title: 'Gemeente Leidschendam-Voorburg',
+  },
+  {
+    className: 'noordoostpolder-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/noordoostpolder-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/noordoostpolder-design-tokens',
+    title: 'Gemeente Noordoostpolder',
+  },
+  {
+    className: 'of-theme',
+    href: 'https://unpkg.com/@open-formulieren/design-tokens/dist/index.css',
+    package: '@open-formulieren/design-tokens',
+    title: 'Open Formulieren',
+  },
+  {
+    className: 'rotterdam-theme',
+    href: 'https://unpkg.com/@gemeente-rotterdam/design-tokens/dist/index.css',
+    package: '@gemeente-rotterdam/design-tokens',
+    title: 'Gemeente Rotterdam',
+  },
+  {
+    className: 'stedebroec-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/stedebroec-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/stedebroec-design-tokens',
+    title: 'Gemeente Stede Broec',
+  },
+  {
+    className: 'tilburg-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/tilburg-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/tilburg-design-tokens',
+    title: 'Gemeente Tilburg',
+  },
+  {
+    className: 'utrecht-theme',
+    href: 'https://unpkg.com/@utrecht/design-tokens/dist/index.css',
+    package: '@utrecht/design-tokens',
+    title: 'Gemeente Utrecht',
+  },
+  {
+    className: 'venray-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/venray-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/venray-design-tokens',
+    title: 'Gemeente Venray',
+  },
+  {
+    className: 'vught-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/vught-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/vught-design-tokens',
+    title: 'Gemeente Vught',
+  },
+  {
+    className: 'westervoort-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/westervoort-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/westervoort-design-tokens',
+    title: 'Gemeente Westervoort',
+  },
+  {
+    className: 'zevenaar-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/zevenaar-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/zevenaar-design-tokens',
+    title: 'Gemeente Zevenaar',
+  },
+  {
+    className: 'zwolle-theme',
+    href: 'https://unpkg.com/@nl-design-system-unstable/zwolle-design-tokens/dist/index.css',
+    package: '@nl-design-system-unstable/zwolle-design-tokens',
+    title: 'Gemeente Zwolle',
+  },
+];
+
+export const AVAILABLE_THEMES = THEMES.map(({ className, title }) => ({ title, value: className }));
--- packages/clippy-storybook/src/templates/GemeenteVoorbeeld/Home.stories.tsx
+++ packages/clippy-storybook/src/templates/GemeenteVoorbeeld/Home.stories.tsx
@@ -2,10 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { GemeenteVoorbeeldHome, type GemeenteVoorbeeldHomeProps } from '@nl-design-system-community/theme-wizard-templates/react';
 import * as React from 'react';
 import '@utrecht/component-library-css';
-import documentation from '../docs/templates/gemeente-voorbeeld-documentatie.md?raw';
+import documentation from './docs/Home.md?raw';
+
+const Home = GemeenteVoorbeeldHome as React.ComponentType<GemeenteVoorbeeldHomeProps>;
 
 const meta = {
-  component: GemeenteVoorbeeldHome as React.ComponentType<GemeenteVoorbeeldHomeProps>,
+  component: Home,
   parameters: {
     docs: {
       description: {
@@ -14,7 +16,8 @@ const meta = {
     },
     layout: 'fullscreen',
   },
-  title: 'Templates/Gemeente Voorbeeld',
+  tags: ['autodocs'],
+  title: 'Templates/Gemeente Voorbeeld/Home',
 } satisfies Meta<GemeenteVoorbeeldHomeProps>;
 
 export default meta;
@@ -22,5 +25,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: 'Gemeente Voorbeeld',
+  name: 'Home',
 };
