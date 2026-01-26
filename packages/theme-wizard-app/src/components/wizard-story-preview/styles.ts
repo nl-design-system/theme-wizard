cc531c5fd97e16be5797a9429c08fa7779e1a0ce
Bart Veneman <bartveneman@gmail.com>
2026-01-26 09:14:32 +0100

    stylelint

--- packages/theme-wizard-app/src/components/wizard-story-preview/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-story-preview/styles.ts
@@ -3,11 +3,11 @@ import { css } from 'lit';
 export default css`
   .wizard-story-preview {
     background-color: white;
+    border-color: rgb(0 0 0 / 10%);
     border-radius: 4px;
-    border-width: 1px;
     border-style: solid;
-    border-color: rgba(0, 0, 0, 0.1);
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px;
+    border-width: 1px;
+    box-shadow: rgb(0 0 0 / 10%) 0 1px 3px 0;
     margin-block-end: 40px;
     margin-block-start: 25px;
     padding-block-end: 30px;
