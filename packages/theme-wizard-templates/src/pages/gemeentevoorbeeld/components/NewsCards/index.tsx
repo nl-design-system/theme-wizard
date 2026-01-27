5aad6b90eea40e0477d9bd507c70bf69d49a3cb1
Michelle <michelleermen@gmail.com>
2026-01-27 07:58:04 +0100

    chore: fix card styles

--- packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/NewsCards/index.tsx
+++ packages/theme-wizard-templates/src/pages/gemeentevoorbeeld/components/NewsCards/index.tsx
@@ -1,7 +1,9 @@
-import { Card, Column as AmsColumn } from '@amsterdam/design-system-react';
+import { Card } from '@amsterdam/design-system-react';
 import { Heading } from '@nl-design-system-candidate/heading-react';
 import { Link } from '@nl-design-system-candidate/link-react';
 import { Paragraph } from '@nl-design-system-candidate/paragraph-react';
+import { IconCalendar } from '@tabler/icons-react';
+import { Icon } from '@utrecht/component-library-react';
 import React from 'react';
 import type { NewsItem } from './types';
 import { Column, Row } from '../Layout';
@@ -18,9 +20,9 @@ const NewsCards = ({ cards }: NewsCardsProps) => (
   <Row columnGap="var(--basis-space-column-xl)" justify="flex-start" rowGap="var(--basis-space-row-xl)" role="list">
     {cards.map((card) => (
       <Column key={`${card.href}-${card.title}`} cols={6} role="listitem">
-        <Card>
+        <Card className="clippy-voorbeeld__link">
           {card.image && (
-            <div style={{ blockSize: '18rem', overflow: 'hidden' }}>
+            <div style={{ blockSize: '18rem', inlineSize: '100%', overflow: 'hidden' }}>
               <Card.Image
                 src={getImageSrc(card.image.src)}
                 alt={card.image.alt}
@@ -29,23 +31,30 @@ const NewsCards = ({ cards }: NewsCardsProps) => (
                   inlineSize: '100%',
                   objectFit: 'cover',
                   objectPosition: card.image.focalPoint || 'center center',
-                  paddingBlockEnd: '1rem',
                 }}
               />
             </div>
           )}
-          <Card.HeadingGroup tagline="Nieuws">
-            <Heading level={3}>
-              <Link href={card.href || '#'}>{card.title}</Link>
-            </Heading>
-          </Card.HeadingGroup>
 
-          <AmsColumn gap="small">
-            <Paragraph>{card.body}</Paragraph>
-            <Paragraph>
+          <div className="clippy-voorbeeld__content">
+            <header>
+              <Card.HeadingGroup tagline="Nieuws">
+                <Heading level={3}>{card.title}</Heading>
+              </Card.HeadingGroup>
+
+              <Icon>
+                <IconCalendar />
+              </Icon>
               <time dateTime={card.dateTime}>{card.date}</time>
-            </Paragraph>
-          </AmsColumn>
+            </header>
+
+            <Paragraph>{card.body}</Paragraph>
+            <footer>
+              <Link href={card.href} aria-label={`Lees meer over ${card.title}`}>
+                Meer lezen
+              </Link>
+            </footer>
+          </div>
         </Card>
       </Column>
     ))}
