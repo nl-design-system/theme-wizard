808c721a00b6955c14c8c2677b73b947de2188aa
Jurgen Beliën <jurgen@voorhoede.nl>
2026-02-02 13:31:54 +0100

    feat(LocalizationMixin): pass default lang to mixin

--- packages/clippy-components/src/lib/LocalizationMixin/index.ts
+++ packages/clippy-components/src/lib/LocalizationMixin/index.ts
@@ -1,26 +1,44 @@
 import { LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
 
-/**
- * Proper typing dictates some specific patterns as defined in Lit documentation.
- * @see https://lit.dev/docs/composition/mixins/#mixins-in-typescript
- */
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 type Constructor<T> = new (...args: any[]) => T;
 
 const DEFAULT_LANG = 'en' as const;
 
+/**
+ * Proper typing dictates some specific patterns as defined in Lit documentation.
+ * @see https://lit.dev/docs/composition/mixins/#mixins-in-typescript
+ */
 declare class LocalizationMixinInterface {
-  DEFAULT_LANG: typeof DEFAULT_LANG;
+  static readonly DEFAULT_LANG: string;
+  get DEFAULT_LANG(): typeof LocalizationMixinInterface.DEFAULT_LANG;
   get lang(): string;
   set lang(value: string);
 }
 
-const LocalizationMixin = <T extends Constructor<LitElement>>(superClass: T) => {
+/**
+ * Ensures that the component has a `lang` property that can be used for other functionality.
+ * On connection, it attempts to determine the language by checking:
+ *   1. The component's `lang` attribute.
+ *   2. The closest ancestor element with a `lang` attribute.
+ *   3. `DEFAULT_LANG`.
+ *
+ * The `lang` property is decorated with `@property()` for Lit reactivity.
+ *
+ * @template T - The base class type, which must extend LitElement.
+ * @param superClass - The base class to extend.
+ * @param defaultLang - The default language (`en` when no value supplied).
+ * @returns A new class extending the base class with localization capabilities.
+ */
+const LocalizationMixin = <T extends Constructor<LitElement>>(superClass: T, defaultLang = DEFAULT_LANG) => {
   class LocalizationMixinClass extends superClass {
-    DEFAULT_LANG = DEFAULT_LANG;
+    static readonly DEFAULT_LANG = defaultLang; // Static because not dependent on the instance
     #lang?: string;
+
+    get DEFAULT_LANG() {
+      return LocalizationMixinClass.DEFAULT_LANG; // Convenience method to abstract the exact class on which the static lives.
+    }
 
     @property()
     override get lang() {
