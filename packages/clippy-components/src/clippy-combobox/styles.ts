994582faed5938babb4416c98a122e4cdf6660c9
Jurgen Beliën <jurgen@voorhoede.nl>
2026-02-05 12:37:08 +0100

    feat(clippy-combobox): clip selected option contents

--- packages/clippy-components/src/clippy-combobox/styles.ts
+++ packages/clippy-components/src/clippy-combobox/styles.ts
@@ -9,6 +9,11 @@ export default css`
   .clippy-combobox__current-option {
     pointer-events: none;
     position: absolute;
+    display: block;
+    // Clip contents when current option exceeds input size
+    inline-size: 100%;
+    white-space: nowrap;
+    overflow: hidden;
   }
   .clippy-combobox__current-option:has(+ input:focus) {
     display: none;
