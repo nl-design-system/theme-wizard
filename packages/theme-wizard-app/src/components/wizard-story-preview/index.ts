ef5b3a2301b0006d4e8b2c871f088807a49892dc
Robbert Broersma <git@robbertbroersma.nl>
2026-02-12 13:20:01 +0100

    chore: remove `preview-theme` wizard from `wizard-story-preview`

--- packages/theme-wizard-app/src/components/wizard-story-preview/index.ts
+++ packages/theme-wizard-app/src/components/wizard-story-preview/index.ts
@@ -1,8 +1,5 @@
-import { consume } from '@lit/context';
 import { LitElement, html } from 'lit';
-import { customElement, state } from 'lit/decorators.js';
-import type Theme from '../../lib/Theme';
-import { themeContext } from '../../contexts/theme';
+import { customElement } from 'lit/decorators.js';
 import styles from './styles';
 
 const tag = 'wizard-story-preview';
@@ -16,20 +13,11 @@ declare global {
 
 @customElement(tag)
 export class WizardStoryPreview extends LitElement {
-  @consume({ context: themeContext, subscribe: true })
-  @state()
-  private readonly theme!: Theme;
-
   static override readonly styles = [styles];
-
-  override connectedCallback(): void {
-    super.connectedCallback();
-    this.shadowRoot?.adoptedStyleSheets.push(this.theme.stylesheet);
-  }
 
   override render() {
     return html`
-      <div class="wizard-story-preview preview-theme">
+      <div class="wizard-story-preview">
         <slot></slot>
       </div>
     `;
