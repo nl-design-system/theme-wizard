49c719dfab539efb60abe832114fd270b2c9164a
Bart Veneman <bartveneman@gmail.com>
2026-02-27 11:01:21 +0100

    typo

--- packages/theme-wizard-app/src/contexts/scraped-tokens.ts
+++ packages/theme-wizard-app/src/contexts/scraped-tokens.ts
@@ -2,6 +2,6 @@ import type { ScrapedDesignToken } from '@nl-design-system-community/css-scraper
 import { createContext } from '@lit/context';
 
 /**
- * Context for scraped colors available to child components
+ * Context for scraped tokens available to child components
  */
 export const scrapedTokensContext = createContext<ScrapedDesignToken[]>('scrapedTokens');
