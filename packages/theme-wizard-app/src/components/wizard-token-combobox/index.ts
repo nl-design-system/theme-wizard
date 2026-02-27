e94ed9d42bb089121d6ca0a3ddc928467d8446bf
Jurgen Beliën <jurgen@voorhoede.nl>
2026-02-27 14:12:43 +0100

    feat(wizard-token-combobox): set label for transient option via valueToQuery

--- packages/theme-wizard-app/src/components/wizard-token-combobox/index.ts
+++ packages/theme-wizard-app/src/components/wizard-token-combobox/index.ts
@@ -111,8 +111,21 @@ export class WizardTokenCombobox extends LocalizationMixin(C) {
     });
   }
 
+  #getOptionForValue(value: Option['value'] | null): Option | undefined {
+    const { $value } = value ?? {};
+    return this.options.find((option) => dequal(option.value.$value, $value));
+  }
+
   override getOptionForValue(value: Option['value'] | null): Option | undefined {
-    return this.options.find((option) => dequal(option.value.$value, value?.$value));
+    const { $type, $value } = value ?? {};
+    const option = this.#getOptionForValue({ $value });
+    if (value && $type && !option && this.allow === 'other') {
+      return {
+        label: this.valueToQuery({ $value }),
+        value,
+      };
+    }
+    return option;
   }
 
   override queryToValue(query: string): Option['value'] | null {
@@ -131,7 +144,7 @@ export class WizardTokenCombobox extends LocalizationMixin(C) {
               return super.queryToValue(query);
           }
         })(query);
-        const option = this.getOptionForValue(value);
+        const option = this.#getOptionForValue(value);
         const $extensions = this.value?.$extensions;
         return option ?? { ...value, $extensions };
       } catch {
@@ -146,7 +159,6 @@ export class WizardTokenCombobox extends LocalizationMixin(C) {
   override valueToQuery({ $value }: Option['value']): string {
     const option = this.getOptionForValue({ $value });
     const stringValue = option?.label || (typeof $value === 'string' ? $value : '');
-
     switch (this.type) {
       case 'color':
         return stringValue || libColor.valueToQuery({ $value });
@@ -160,10 +172,11 @@ export class WizardTokenCombobox extends LocalizationMixin(C) {
   }
 
   renderPreview(option: Option) {
-    switch (this.type) {
+    switch (option.value.$type) {
       case 'color':
         return libColor.preview(option);
       case 'fontFamily':
+        // TODO fix type safety by making sure option type is inferred from `option.value.$type`
         return libFontFamily.preview(option as Option & { value: FontFamilyToken });
       case 'dimension':
       case 'number':
