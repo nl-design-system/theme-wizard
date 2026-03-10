a4b6e7e6bfd26b2b53626f3bd482ef27221ae858
Yolijn <mail@yolijn.com>
2026-03-11 00:11:39 +0100

    feat: improve styles

--- packages/theme-wizard-app/src/components/wizard-token-presets/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-token-presets/styles.ts
@@ -6,6 +6,30 @@ export const wizardTokenCSS = css`
   }
 
   button {
-    appearance: none;
+    border-width: var(--basis-border-width-sm);
+    border-color: var(--basis-color-accent-1-border-subtle);
+    border-style: solid;
+    background-color: var(--basis-color-accent-1-bg-subtle);
+    color: var(--basis-color-accent-1-color-default);
+    padding-inline: var(--basis-space-inline-2xl);
+    padding-block: var(--basis-space-block-2xl);
+    display: block;
+    width: 100%;
+    margin-block-end: var(--basis-space-block-xl);
+    text-align: start;
+  }
+
+  button:hover {
+    border-color: var(--basis-color-accent-1-border-hover);
+    border-style: solid;
+    background-color: var(--basis-color-accent-1-bg-hover);
+    color: var(--basis-color-accent-1-color-hover);
+  }
+
+  button:active {
+    border-color: var(--basis-color-accent-1-border-active);
+    border-style: solid;
+    background-color: var(--basis-color-accent-1-bg-active);
+    color: var(--basis-color-accent-1-color-active);
   }
 `;
