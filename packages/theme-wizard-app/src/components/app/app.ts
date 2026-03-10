2766a85e8a6bccf6d7d1f809134b3dd70b69cb82
Yolijn <mail@yolijn.com>
2026-03-10 20:05:05 +0100

    feat: add component preset logic

--- packages/theme-wizard-app/src/components/app/app.ts
+++ packages/theme-wizard-app/src/components/app/app.ts
@@ -6,14 +6,23 @@ import { scrapedTokensContext } from '../../contexts/scraped-tokens';
 import { themeContext } from '../../contexts/theme';
 import PersistentStorage from '../../lib/PersistentStorage';
 import Theme from '../../lib/Theme';
+import { tokensToUpdateMany } from '../../lib/Theme/lib';
 import { EXTENSION_TOKEN_STAGED, StagedDesignToken } from '../../utils/types';
 import { WizardColorscaleInput } from '../wizard-colorscale-input';
 import { WizardScraper } from '../wizard-scraper';
 import { WizardTokenCombobox } from '../wizard-token-combobox';
 import { WizardTokenInput } from '../wizard-token-input';
+import { WizardTokenPreset } from '../wizard-token-presets';
 
 const tag = 'theme-wizard-app';
 
+export type ThemeUpdateEvent = CustomEvent<{ theme: Theme }>;
+
+declare global {
+  interface DocumentEventMap {
+    'theme-update': ThemeUpdateEvent;
+  }
+}
 declare global {
   interface HTMLElementTagNameMap {
     [tag]: App;
@@ -80,6 +89,7 @@ export class App extends LitElement {
   readonly #forceUpdateTokens = () => {
     this.theme = this.theme.clone();
     this.requestUpdate();
+    this.dispatchEvent(new CustomEvent('theme-update', { bubbles: true, detail: { theme: this.theme } }));
   };
 
   readonly #handleScrapeDone = (event: Event) => {
@@ -104,8 +114,9 @@ export class App extends LitElement {
   };
 
   readonly #handleTokenChange = async (event: Event) => {
+    console.log(event);
     const target = event.composedPath().shift(); // @see https://lit.dev/docs/components/events/#shadowdom-retargeting
-    if (!(target instanceof WizardTokenInput || target instanceof WizardTokenCombobox)) return;
+
     if (target instanceof WizardColorscaleInput) {
       const scaleColors = Object.values(target.value);
       const reversedScale = scaleColors.toReversed();
@@ -129,6 +140,10 @@ export class App extends LitElement {
       this.theme.updateAt(target.name, target.value?.$value);
     } else if (target instanceof WizardTokenInput) {
       this.theme.updateAt(target.name, target.value);
+    } else if (target instanceof WizardTokenPreset) {
+      this.theme.updateMany(tokensToUpdateMany(target.value));
+    } else {
+      return;
     }
     this.#forceUpdateTokens();
     this.#themeStorage.setJSON(this.theme.tokens);
--- packages/theme-wizard-app/src/lib/Theme/lib.ts
+++ packages/theme-wizard-app/src/lib/Theme/lib.ts
@@ -1,5 +1,7 @@
-import { BaseDesignTokenIdentifier, isTokenLike } from '@nl-design-system-community/design-tokens-schema';
+import { BaseDesignTokenIdentifier, isTokenLike, walkTokens } from '@nl-design-system-community/design-tokens-schema';
 import { DesignToken, DesignTokens } from 'style-dictionary/types';
+import { TokenGroup } from '../../components/wizard-reset-theme/reset-css';
+
 export const tokenPathToCSSCustomProperty = (tokenPath: BaseDesignTokenIdentifier[]): string =>
   '--' + tokenPath.join('-');
 
@@ -28,3 +30,34 @@ export const refToCssVariable = (value: string): string =>
   value.replaceAll(/\{([^}]+)\}/g, (_, tokenName) => {
     return `var(--${tokenName.replaceAll('.', '-')})`;
   });
+
+export const tokensToUpdateMany = (tokens: TokenGroup) => {
+  const updateMany: { path: string; value: DesignToken['$value'] }[] = [];
+
+  walkTokens(tokens, (token, currentPath) => {
+    const path = currentPath.join('.');
+    const value = token.$value;
+
+    updateMany.push({ path, value });
+  });
+
+  return updateMany;
+};
+
+/**
+ * Replace `{example.component.property}` with `var(--example-component-property)`
+ */
+
+export const tokensToStyle = (tokens: TokenGroup) => {
+  const style: { [index: string]: string } = {};
+
+  walkTokens(tokens, (token, path) => {
+    if (typeof token.$value === 'string') {
+      const cssProperty = `--${path.join('-')}`;
+      const cssValue = refToCssVariable(token.$value);
+      style[cssProperty] = cssValue;
+    }
+  });
+
+  return style;
+};
