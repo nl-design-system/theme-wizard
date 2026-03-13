07515d22bc413e6b579b3926c59529887526dbdf
Michelle <michelleermen@gmail.com>
2026-03-13 11:57:31 +0100

    fix: always import button/heading/paragraph.

--- packages/theme-wizard-website/src/components/story-wizard/componentStyles.ts
+++ packages/theme-wizard-website/src/components/story-wizard/componentStyles.ts
@@ -1,16 +1,15 @@
 import type { components } from '@/lib/components';
 
+import '@nl-design-system-candidate/button-css/button.css';
+import '@nl-design-system-candidate/heading-css/heading.css';
+import '@nl-design-system-candidate/paragraph-css/paragraph.css';
+
 type ComponentId = keyof typeof components;
 type StyleLoader = () => Promise<unknown>;
 
-const sharedStyleLoaders: StyleLoader[] = [
-  () => import('@nl-design-system-candidate/button-css/button.css'),
-  () => import('@nl-design-system-candidate/heading-css/heading.css'),
-  () => import('@nl-design-system-candidate/paragraph-css/paragraph.css'),
-];
 const componentStyleLoaders: Partial<Record<ComponentId, StyleLoader[]>> = {};
 
 export async function loadStoryWizardComponentStyles(componentId: ComponentId) {
-  const loaders = [...sharedStyleLoaders, ...(componentStyleLoaders[componentId] ?? [])];
+  const loaders = componentStyleLoaders[componentId] ?? [];
   await Promise.all(loaders.map((loadStyles) => loadStyles()));
 }
