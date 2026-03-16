50776cd851eb4e4e831956636f30eee0b3b2f980
Bart Veneman <bartveneman@gmail.com>
2026-03-16 09:35:08 +0100

    feat: start redesign of staging tokens page

--- packages/theme-wizard-app/src/components/wizard-container/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-container/styles.ts
@@ -6,27 +6,27 @@ export default css`
   }
 
   .wizard-container {
-    margin-inline: auto;
     padding-inline: var(--basis-space-inline-xl);
+    max-inline-size: 100%;
   }
 
   .wizard-container--page {
-    max-inline-size: var(--basis-page-max-inline-size);
+    inline-size: var(--basis-page-max-inline-size);
   }
 
   .wizard-container--sm {
-    max-inline-size: 20rem;
+    inline-size: 20rem;
   }
 
   .wizard-container--md {
-    max-inline-size: 33rem;
+    inline-size: 33rem;
   }
 
   .wizard-container--lg {
-    max-inline-size: 44rem;
+    inline-size: 44rem;
   }
 
   .wizard-container--xl {
-    max-inline-size: 55rem;
+    inline-size: 55rem;
   }
 `;
