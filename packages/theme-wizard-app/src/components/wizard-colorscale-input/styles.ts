d70514664a33970074884cae57abd6aca5523b37
Bart Veneman <bartveneman@gmail.com>
2026-03-19 13:05:55 +0100

    fix: set correct size and radius on colorscale preview

--- packages/theme-wizard-app/src/components/wizard-colorscale-input/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-colorscale-input/styles.ts
@@ -1,6 +1,12 @@
 import { css } from 'lit';
 
 export default css`
+  :host {
+    --nl-color-sample-border-radius: var(--basis-border-radius-round);
+    --nl-color-sample-block-size: var(--basis-size-xs);
+    --nl-color-sample-inline-size: var(--basis-size-xs);
+  }
+
   .wizard-colorscale-input__input input {
     inline-size: 100%;
   }
