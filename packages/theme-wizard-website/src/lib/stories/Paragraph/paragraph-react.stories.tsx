bc82fc066d37ceea1353235eea30f04c0db7a6d3
Michelle <michelleermen@gmail.com>
2026-03-20 12:01:19 +0100

    feat: add paragraph preset stories

---
+++ packages/theme-wizard-website/src/lib/stories/Paragraph/paragraph-react.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import css from '@nl-design-system-candidate/paragraph-css/paragraph.css?inline';
+import { Paragraph, type ParagraphProps } from '@nl-design-system-candidate/paragraph-react';
+import tokens from '@nl-design-system-candidate/paragraph-tokens';
+
+const meta = {
+  id: 'paragraph',
+  component: Paragraph,
+  parameters: {
+    css: [css],
+    tokens,
+  },
+  title: 'Paragraph',
+} satisfies Meta<typeof Paragraph>;
+
+export default meta;
+
+export * from './paragraph-react.preset.stories';
+export * from './paragraph-react.advanced-stories';
+
+type Story = StoryObj<ParagraphProps>;
+
+export const ParagraphStory: Story = {
+  name: 'Paragraph',
+  args: {
+    children: 'Op brute wĳze ving de schooljuf de quasi-kalme lynx.',
+    purpose: undefined,
+  },
+};
+
+export const LeadParagraphStory: Story = {
+  name: 'Lead Paragraph',
+  args: {
+    children: 'Op brute wĳze ving de schooljuf de quasi-kalme lynx.',
+    purpose: 'lead',
+  },
+};
