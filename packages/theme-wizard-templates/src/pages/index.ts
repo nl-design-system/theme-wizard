baefac344605b48bd521f54003533afede4a63d7
Bart Veneman <bartveneman@gmail.com>
2026-03-31 15:30:03 +0200

    feat: add implementations for multi-step forms in templates

--- packages/theme-wizard-templates/src/pages/index.ts
+++ packages/theme-wizard-templates/src/pages/index.ts
@@ -6,6 +6,8 @@ export { default as MyEnvironmentOverviewPage } from './my-environment/overview.
 export { default as GemeenteVoorbeeldPage } from './gemeentevoorbeeld/page.astro';
 export { default as MultiStepFormStep1Page } from './multi-step-form/step-1.astro';
 export { default as MultiStepFormStep2Page } from './multi-step-form/step-2.astro';
+export { default as MultiStepFormStep3Page } from './multi-step-form/step-3.astro';
+export { default as MultiStepFormStep4Page } from './multi-step-form/step-4.astro';
 export { default as PreviewComponentsCollage1Page } from './preview-components/collage-1.astro';
 export { default as PreviewComponentsColorSwatchPage } from './preview-components/color-swatch.astro';
 export { default as PreviewComponentsFontPropertiesPage } from './preview-components/font-properties.astro';
