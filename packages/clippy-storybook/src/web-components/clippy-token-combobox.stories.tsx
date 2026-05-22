6fe5306251a4a21bb591da9dd13912caf3f3a857
Bram Smulders <bram@supple.works>
2026-05-22 11:43:44 +0200

    feat: start with showing options

--- packages/clippy-storybook/src/web-components/clippy-token-combobox.stories.tsx
+++ packages/clippy-storybook/src/web-components/clippy-token-combobox.stories.tsx
@@ -1,32 +1,50 @@
 import type { ClippyTokenComboboxOption } from '@nl-design-system-community/clippy-components/clippy-token-combobox';
 import '@nl-design-system-community/clippy-components/clippy-token-combobox';
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import {
+  type TokenReference,
+  type ColorToken,
+  walkColors,
+  EXTENSION_RESOLVED_AS,
+  isRef,
+  extractRef,
+  resolveRef,
+} from '@nl-design-system-community/design-tokens-schema';
+import startTokens from '@nl-design-system-unstable/start-design-tokens/dist/tokens.json';
 import { html } from 'lit';
 import React from 'react';
 import { templateToHtml } from '../utils/templateToHtml';
 
-const OPTIONS: ClippyTokenComboboxOption[] = [
-  {
-    label: 'test.token.1',
-    value: { type: 'color', value: '#000000' },
+const colorTokens: Array<{ token: ColorToken; path: string[] }> = [];
+
+walkColors(startTokens, (token, path) => {
+  colorTokens.push({
+    path,
+    token,
+  });
+});
+
+const colorOptions = colorTokens.map(({ path, token }) => ({
+  label: path.join('.'),
+  value: {
+    $type: token.$type,
+    $value: isRef(token.$value) ? resolveRef(startTokens, token.$value) : token.$value,
   },
-  {
-    label: 'test.token.2',
-    value: { type: 'color', value: '#ff0000' },
-  },
-];
+}));
+
+console.log('colorOptions', colorOptions);
 
 interface TokenComboboxStoryArgs {
-  options: typeof OPTIONS;
+  options: ClippyTokenComboboxOption[];
 }
 
 const createTemplate = (options: TokenComboboxStoryArgs['options']) =>
-  html`<clippy-token-combobox options=${JSON.stringify(options, null, 2)}></clippy-token-combobox>`;
+  html`<clippy-token-combobox type="color" options=${JSON.stringify(options, null, 2)}></clippy-token-combobox>`;
 
 const meta = {
   id: 'clippy-token-combobox',
   args: {
-    options: OPTIONS,
+    options: colorOptions,
   },
   parameters: {
     docs: {
