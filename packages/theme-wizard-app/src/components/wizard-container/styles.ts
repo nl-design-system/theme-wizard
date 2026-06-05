64c97a1a7863bffaca95f1ea250b04c29a22d005
Bart Veneman <bartveneman@gmail.com>
2026-06-05 11:10:58 +0200

    fix: no div wrapper for wizard-container, fixes sizing

--- packages/theme-wizard-app/src/components/wizard-container/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-container/styles.ts
@@ -5,38 +5,36 @@ export default css`
     display: block;
   }
 
-  .wizard-container {
+  :host {
     box-sizing: border-box;
-    display: block;
-    inline-size: 100%;
   }
 
-  .wizard-container--page {
+  :host([size='page']) {
     max-inline-size: var(--basis-page-max-inline-size);
     padding-inline: var(--basis-space-inline-xl);
   }
 
-  .wizard-container--sm {
+  :host([size='sm']) {
     max-inline-size: 20rem;
   }
 
-  .wizard-container--md {
+  :host([size='md']) {
     max-inline-size: 33rem;
   }
 
-  .wizard-container--lg {
+  :host([size='lg']) {
     max-inline-size: 44rem;
   }
 
-  .wizard-container--xl {
+  :host([size='xl']) {
     max-inline-size: 55rem;
   }
 
-  .wizard-container--2xl {
+  :host([size='2xl']) {
     max-inline-size: 72rem;
   }
 
-  .wizard-container--3xl {
+  :host([size='3xl']) {
     max-inline-size: 96rem;
   }
 `;
