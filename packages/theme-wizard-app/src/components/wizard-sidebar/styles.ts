de84e1132f1d1cca09d4260ec7088b6b05842180
Bram Smulders <bram@supple.works>
2026-06-18 14:07:07 +0200

    wip: cleanup

--- packages/theme-wizard-app/src/components/wizard-sidebar/styles.ts
+++ packages/theme-wizard-app/src/components/wizard-sidebar/styles.ts
@@ -3,7 +3,6 @@ import { css } from 'lit';
 export default css`
   :host(:not([hidden])) {
     background-color: var(--basis-color-accent-1-bg-subtle);
-    /*block-size: 100%;*/
     box-sizing: border-box;
     display: grid;
     grid-template-rows: auto 1fr auto;
