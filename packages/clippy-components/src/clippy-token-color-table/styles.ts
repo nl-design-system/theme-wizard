702e5feb3a96e34089a95830d286fe8562e65507
Bram Smulders <bram@supple.works>
2026-07-30 14:28:18 +0200

    chore: restructure CSS

--- packages/clippy-components/src/clippy-token-color-table/styles.ts
+++ packages/clippy-components/src/clippy-token-color-table/styles.ts
@@ -3,32 +3,42 @@ import { css } from 'lit';
 export default css`
   :host(:not([hidden])) {
     display: block;
+  }
 
+  :host {
     --nl-color-sample-block-size: var(--basis-pointer-target-min-block-size, 44px);
-    --nl-color-sample-inline-size: var(--basis-pointer-target-min-block-size, 44px);
+    --nl-color-sample-inline-size: 4rem;
+    --utrecht-table-cell-padding-inline-end: 0.5rem;
+    --utrecht-table-cell-padding-inline-start: 0.5rem;
   }
 
   .clippy-token-color-table__header-cell {
-    &[scope='row'],
-    &[scope='col'] {
+    &[scope='col'],
+    &[scope='row'] {
       white-space: nowrap;
     }
-    &[scope='row'] {
-      --_utrecht-table-header-cell-vertical-align: middle;
-      min-inline-size: 5rem;
-    }
+
     &[scope='col'] {
       --utrecht-table-header-cell-font-weight: var(--basis-text-font-weight-default);
+
+      font-size: var(--basis-text-font-size-sm);
+      line-height: var(--basis-text-line-height-sm);
+      min-inline-size: 5rem;
+    }
+
+    &[scope='row'] {
+      --_utrecht-table-header-cell-vertical-align: middle;
+
       min-inline-size: 5rem;
     }
   }
 
   .clippy-token-color-table__mastheader {
-    display: block;
-    padding-block-end: var(--utrecht-table-cell-padding-block-end, 0);
     border-block-end-color: var(--utrecht-table-row-border-block-end-color, transparent);
     border-block-end-style: solid;
     border-block-end-width: var(--utrecht-table-row-border-block-end-width, 0);
+    display: block;
+    padding-block-end: var(--utrecht-table-cell-padding-block-end, 0);
   }
 
   .clippy-token-color-table__cell {
