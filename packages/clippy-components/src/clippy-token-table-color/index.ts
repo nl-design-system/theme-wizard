a666967299ca6c07148c87b75b19ee6f1a22c5ec
Bram Smulders <bram@supple.works>
2026-07-31 11:36:57 +0200

    wip: render rudimentary modals

--- packages/clippy-components/src/clippy-token-table-color/index.ts
+++ packages/clippy-components/src/clippy-token-table-color/index.ts
@@ -1,9 +1,14 @@
+import codeCss from '@nl-design-system-candidate/code-css/code.css?inline';
+import dataBadgeCss from '@nl-design-system-candidate/data-badge-css/data-badge.css?inline';
 import { safeCustomElement } from '@src/lib/decorators';
 import tableCss from '@utrecht/table-css/dist/index.css?inline';
+import Color from 'colorjs.io';
 import { LitElement, html, unsafeCSS } from 'lit';
-import { property } from 'lit/decorators.js';
 import '../clippy-color-sample';
-import type { ColorGroup } from './types';
+import '../clippy-modal';
+import { property } from 'lit/decorators.js';
+import type { ColorEntry, ColorGroup } from './types';
+import { ClippyModal } from '../clippy-modal';
 import srOnly from '../lib/sr-only';
 import styles from './styles';
 
@@ -19,10 +24,77 @@ declare global {
 
 @safeCustomElement(tag)
 export class ClippyTokenTableColor extends LitElement {
-  static override readonly styles = [unsafeCSS(tableCss), styles, srOnly];
+  static override readonly styles = [unsafeCSS(dataBadgeCss), unsafeCSS(codeCss), unsafeCSS(tableCss), styles, srOnly];
 
   @property({ type: Array })
   groups: ColorGroup[] = [];
+
+  #openDialog({ dialogId }: { dialogId: string }) {
+    const dialog = this.shadowRoot?.getElementById(dialogId) as ClippyModal;
+    if (!dialog) return;
+    dialog.open();
+  }
+
+  #renderDialog({ dialogId, entry }: { dialogId: string; entry: ColorEntry }) {
+    const color = new Color(entry.displayValue);
+    return html`
+      <clippy-modal id="${dialogId}" title="${entry.tokenId}" actions="none">
+        <clippy-heading level=${3}><mark>Voorbeeld</mark></clippy-heading>
+        <clippy-color-sample color=${entry.displayValue}></clippy-color-sample>
+        <dl>
+          <dt>Token type</dt>
+          <dd>
+            <code class="nl-code">color</code>
+          </dd>
+          <dt>Token ID</dt>
+          <dd>
+            <span class="nl-data-badge">${entry.tokenId}</span>
+          </dd>
+          <dt>CSS Variable</dt>
+          <dd>
+            <code class="nl-code">${`--${entry.tokenId.replaceAll('.', '-')}`}</code>
+          </dd>
+          <dt><mark>Value</mark></dt>
+          <dd>
+            <code class="nl-code">${entry.displayValue}</code>
+          </dd>
+          <dt>OKLCH</dt>
+          <dd>
+            <code class="nl-code">${color.toString({ format: 'oklch' })}</code>
+          </dd>
+          <dt>P3 Color</dt>
+          <dd>
+            <code class="nl-code">${color.toString({ format: 'color' })}</code>
+          </dd>
+          <dt>RGB</dt>
+          <dd>
+            <code class="nl-code">${color.toString({ format: 'rgb' })}</code>
+          </dd>
+        </dl>
+
+        <clippy-heading level=${3}>
+          <mark>Waar wordt deze token gebruikt?</mark>
+          <data>(${entry.usage.length}&times;)</data>
+        </clippy-heading>
+
+        ${
+          entry.usage.length > 0
+            ? html`
+                <ul>
+                  ${entry.usage.map(
+                    (referrer) => html`
+                      <li>
+                        <span class="nl-data-badge">${referrer}</span>
+                      </li>
+                    `,
+                  )}
+                </ul>
+              `
+            : html` <utrecht-paragraph><mark>Deze token wordt niet gebruikt</mark></utrecht-paragraph> `
+        }
+      </clippy-modal>
+    `;
+  }
 
   override render() {
     return html`<div class="utrecht-table-container utrecht-table-container--overflow-inline">
@@ -95,13 +167,16 @@ export class ClippyTokenTableColor extends LitElement {
                 <th class="clippy-token-table-color__header-cell | utrecht-table__header-cell" scope="row">
                   ${groupName}
                 </th>
-                ${colorEntries.map(
-                  (entry) =>
-                    html`<td class="clippy-token-table-color__cell | utrecht-table__cell">
+                ${colorEntries.map((entry) => {
+                  const dialogId = `token-dialog-${entry.tokenId.replaceAll('.', '-')}`;
+                  return html`<td class="clippy-token-table-color__cell | utrecht-table__cell">
+                    <button type="button" @click=${() => this.#openDialog({ dialogId })}>
                       <clippy-color-sample color="${entry.displayValue}"></clippy-color-sample>
                       <span class="sr-only">${entry.tokenId}</span>
-                    </td>`,
-                )}
+                    </button>
+                    ${this.#renderDialog({ dialogId, entry })}
+                  </td>`;
+                })}
               </tr>`,
           )}
         </tbody>
