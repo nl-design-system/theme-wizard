1d8cf5ade30e9e612d980c7fec5d83dacf871de8
Bart Veneman <bartveneman@gmail.com>
2026-08-04 16:47:45 +0200

    rm defaults from jsdoc

--- packages/clippy-components/src/clippy-graph-paper/index.ts
+++ packages/clippy-components/src/clippy-graph-paper/index.ts
@@ -9,11 +9,11 @@ const tag = 'clippy-graph-paper';
  *
  * @slot - Default slot for content
  *
- * @cssprop [--clippy-graph-paper-cell-size=8px] - Size of one grid cell
- * @cssprop [--clippy-graph-paper-line-size=1px] - Thickness of the grid lines
- * @cssprop [--clippy-graph-paper-minor-line-color=rgba(0, 0, 0, 0.06)] - Color of the minor (per-cell) grid lines
- * @cssprop [--clippy-graph-paper-major-line-color=rgba(0, 0, 0, 0.06)] - Color of the major grid lines
- * @cssprop [--clippy-graph-paper-major-line-interval=4] - Number of cells between major grid lines
+ * @cssprop [--clippy-graph-paper-cell-size] - Size of one grid cell
+ * @cssprop [--clippy-graph-paper-line-size] - Thickness of the grid lines
+ * @cssprop [--clippy-graph-paper-minor-line-color] - Color of the minor (per-cell) grid lines
+ * @cssprop [--clippy-graph-paper-major-line-color] - Color of the major grid lines
+ * @cssprop [--clippy-graph-paper-major-line-interval] - Number of cells between major grid lines
  */
 @safeCustomElement(tag)
 export class ClippyGraphPaper extends LitElement {
