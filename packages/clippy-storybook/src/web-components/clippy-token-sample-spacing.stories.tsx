24cd858c5963d4377cdd406aeef57dceba0b34e2
Bram Smulders <bram@supple.works>
2026-08-05 16:49:00 +0200

    docs: stories

--- packages/clippy-storybook/src/web-components/clippy-token-sample-spacing.stories.tsx
+++ packages/clippy-storybook/src/web-components/clippy-token-sample-spacing.stories.tsx
@@ -13,6 +13,10 @@ type TableColorStoryArgs = {
 
 const meta = {
   id: 'clippy-token-sample-spacing',
+  args: {
+    concept: 'inline',
+    size: '1rem',
+  },
   argTypes: {
     concept: {
       control: { type: 'select' },
@@ -22,7 +26,8 @@ const meta = {
   parameters: {
     docs: {
       description: {
-        component: '`<clippy-token-sample-spacing>` is een component om concepten rond spacing-tokens te illustreren.',
+        component:
+          '`<clippy-token-sample-spacing>` is a component to illustrate spacing tokens and their concepts within the NL Design System.',
       },
     },
   },
@@ -36,9 +41,66 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  name: 'Basis token sample spacing',
+  name: 'Default inline',
+};
+
+export const Size: Story = {
+  name: 'Size',
   args: {
-    concept: 'inline',
-    size: '2rem',
+    size: '3rem',
   },
+};
+
+export const ConceptBlock: Story = {
+  name: 'Concept: block',
+  args: {
+    concept: 'block',
+  },
+};
+
+export const ConceptText: Story = {
+  name: 'Concept: text',
+  args: {
+    concept: 'text',
+  },
+};
+
+export const ConceptColumn: Story = {
+  name: 'Concept: column',
+  args: {
+    concept: 'column',
+  },
+};
+
+export const ConceptRow: Story = {
+  name: 'Concept: row',
+  args: {
+    concept: 'row',
+  },
+};
+
+export const SlotLabel: Story = {
+  name: 'Slot: `label`',
+  render: (args: TableColorStoryArgs) =>
+    React.createElement('clippy-token-sample-spacing', args, React.createElement('span', { slot: 'label' }, 'item')),
+};
+
+export const SlotLabelStart: Story = {
+  name: 'Slot: `label-start`',
+  args: {
+    concept: 'row',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Slot for a second label placed at the start, only available to `row`, `column` and `text` concepts.',
+      },
+    },
+  },
+  render: (args: TableColorStoryArgs) =>
+    React.createElement(
+      'clippy-token-sample-spacing',
+      args,
+      React.createElement('span', { slot: 'label-start' }, 'item'),
+    ),
 };
