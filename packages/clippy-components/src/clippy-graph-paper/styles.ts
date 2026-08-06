6f47edeb96c1be77b555c236d9577d175afd6ff0
Bart Veneman <bartveneman@gmail.com>
2026-08-06 16:46:22 +0200

    add more stories, process review feedback

--- packages/clippy-components/src/clippy-graph-paper/styles.ts
+++ packages/clippy-components/src/clippy-graph-paper/styles.ts
@@ -2,10 +2,17 @@ import { css } from 'lit';
 
 export default css`
   :host {
-    --_clippy-graph-paper-cell-size: var(--clippy-graph-paper-cell-size, 8px);
+    --_clippy-graph-paper-cell-size: var(--clippy-graph-paper-cell-size, 1lh);
     --_clippy-graph-paper-line-size: var(--clippy-graph-paper-line-size, 1px);
-    --_clippy-graph-paper-minor-line-color: var(--clippy-graph-paper-minor-line-color, rgb(0 0 0 / 6%));
-    --_clippy-graph-paper-major-line-color: var(--clippy-graph-paper-major-line-color, rgb(0 0 0 / 6%));
+    --_clippy-graph-paper-line-color: var(--clippy-graph-paper-line-color, rgb(0 0 0 / 6%));
+    --_clippy-graph-paper-minor-line-color: var(
+      --clippy-graph-paper-minor-line-color,
+      var(--_clippy-graph-paper-line-color)
+    );
+    --_clippy-graph-paper-major-line-color: var(
+      --clippy-graph-paper-major-line-color,
+      var(--_clippy-graph-paper-line-color)
+    );
     --_clippy-graph-paper-major-line-interval: var(--clippy-graph-paper-major-line-interval, 4);
 
     background-image:
@@ -29,7 +36,7 @@ export default css`
         var(--_clippy-graph-paper-major-line-color) var(--_clippy-graph-paper-line-size),
         transparent var(--_clippy-graph-paper-line-size)
       );
-    background-position: -1px -1px;
+    background-position: calc(var(--_clippy-graph-paper-line-size) * -1) calc(var(--_clippy-graph-paper-line-size) * -1);
     background-size:
       var(--_clippy-graph-paper-cell-size) var(--_clippy-graph-paper-cell-size),
       var(--_clippy-graph-paper-cell-size) var(--_clippy-graph-paper-cell-size),
--- packages/clippy-storybook/src/web-components/clippy-graph-paper.stories.tsx
+++ packages/clippy-storybook/src/web-components/clippy-graph-paper.stories.tsx
@@ -4,8 +4,6 @@ import { html } from 'lit';
 import React from 'react';
 import { templateToHtml } from '../utils/templateToHtml';
 
-const createTemplate = () => html`<clippy-graph-paper>Example content</clippy-graph-paper>`;
-
 const meta = {
   id: 'clippy-graph-paper',
   argTypes: {
@@ -66,17 +64,120 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const createTemplate = () =>
+  html`<clippy-graph-paper style="display: block; padding-block: 2lh; padding-inline: 3ch;"
+    >Example content</clippy-graph-paper
+  >`;
+
 export const Default: Story = {
   name: 'Default',
   parameters: {
     docs: {
       source: {
-        transform: () => {
-          const template = createTemplate();
-          return templateToHtml(template);
-        },
+        transform: () => templateToHtml(createTemplate()),
         type: 'code',
       },
     },
   },
+  render: () => React.createElement('div', { dangerouslySetInnerHTML: { __html: templateToHtml(createTemplate()) } }),
+};
+
+const createoldSchoolMathTemplate = () => html`
+  <clippy-graph-paper
+    style="
+      --clippy-graph-paper-cell-size: .5lh;
+      --clippy-graph-paper-major-line-interval: 4;
+      --clippy-graph-paper-line-color: rgb(0 0 0/ 6%);
+      --clippy-graph-paper-major-line-color: oklch(from deepskyblue l calc(c * .5) h / 25%);
+
+      display: block;
+      padding-block: 2.5lh;
+      padding-inline: 6ch;
+    "
+    >1 + 1 = 2</clippy-graph-paper
+  >
+`;
+
+export const OldSchoolMathPaper: Story = {
+  name: 'Old-school Math Paper',
+  parameters: {
+    docs: {
+      source: {
+        transform: () => templateToHtml(createoldSchoolMathTemplate()),
+        type: 'code',
+      },
+    },
+  },
+  render: () =>
+    React.createElement('div', { dangerouslySetInnerHTML: { __html: templateToHtml(createoldSchoolMathTemplate()) } }),
+};
+
+// Example SVG taken from https://nldesignsystem.nl/componenten/ with the SVG's background graph paper removed.
+const createNlDesignSystemComponentCardTemplate = () => html`
+  <div style="container-type: inline-size; max-inline-size: 500px;">
+    <clippy-graph-paper
+      style="
+        --clippy-graph-paper-cell-size: calc((100 / 12 * 1cqi) + 1px);
+        --clippy-graph-paper-major-line-interval: calc(infinity);
+
+        display: block;
+        padding-block: 2lh;
+        padding-inline: 3ch;
+      "
+    >
+      <svg
+        style="width: 100%; height: auto;"
+        width="960"
+        height="540"
+        viewBox="0 0 960 540"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+      >
+        <g>
+          <rect
+            x="127"
+            y="229"
+            width="330"
+            height="84"
+            rx="16"
+            fill="var(--ma-component-illustration-color, #666)"
+          ></rect>
+          <rect
+            x="172"
+            y="263"
+            width="240"
+            height="16"
+            rx="8"
+            fill="var(--ma-component-illustration-background-color, white)"
+          ></rect>
+          <rect
+            x="505"
+            y="230"
+            width="326"
+            height="80"
+            rx="14"
+            fill="var(--ma-component-illustration-background-color, white)"
+            stroke="var(--ma-component-illustration-color, #666)"
+            stroke-width="4"
+          ></rect>
+        </g>
+      </svg>
+    </clippy-graph-paper>
+  </div>
+`;
+
+export const NlDesignSystemComponentCard: Story = {
+  name: 'NL Design System Component Card',
+  parameters: {
+    docs: {
+      source: {
+        transform: () => templateToHtml(createNlDesignSystemComponentCardTemplate()),
+        type: 'code',
+      },
+    },
+  },
+  render: () =>
+    React.createElement('div', {
+      dangerouslySetInnerHTML: { __html: templateToHtml(createNlDesignSystemComponentCardTemplate()) },
+    }),
 };
