7b1ccd2f2b1cc4b0f6b293246d73d874b663836b
Bram Smulders <bram@supple.works>
2026-08-07 14:45:42 +0200

    wip: structure

--- packages/clippy-components/src/clippy-token-sample-typography/index.ts
+++ packages/clippy-components/src/clippy-token-sample-typography/index.ts
@@ -1,5 +1,8 @@
+import paragraphStyles from '@nl-design-system-candidate/paragraph-css/paragraph.css?inline';
 import { safeCustomElement } from '@src/lib/decorators';
-import { LitElement, html } from 'lit';
+import { LitElement, html, unsafeCSS } from 'lit';
+import { property } from 'lit/decorators.js';
+import '../clippy-html-image';
 import styles from './styles';
 
 const tag = 'clippy-token-sample-typography';
@@ -15,9 +18,20 @@ declare global {
  */
 @safeCustomElement(tag)
 export class ClippyTokenSampleTypography extends LitElement {
-  static override readonly styles = [styles];
+  static override readonly styles = [unsafeCSS(paragraphStyles), styles];
+
+  @property({ type: String }) size: string = '';
+  @property({ type: String }) family: string = '';
+  @property({ type: String }) color: string = '';
+  @property({ type: Boolean }) truncate: boolean = false;
 
   override render() {
-    return html`clippy-token-sample-typography`;
+    return html`
+      <clippy-html-image>
+        <p class="clippy-token-sample-typography__paragraph | nl-paragraph">
+          <slot>Op brute wijze ving de schooljuf de quasi-kalme lynx.</slot>
+        </p>
+      </clippy-html-image>
+    `;
   }
 }
--- packages/clippy-components/src/clippy-token-sample-typography/styles.ts
+++ packages/clippy-components/src/clippy-token-sample-typography/styles.ts
@@ -4,4 +4,19 @@ export default css`
   :host(:not([hidden])) {
     display: block;
   }
+
+  :host {
+    --nl-paragraph-font-size: var(--clippy-token-sample-text-font-size, var(--basis-text-font-size-md));
+    --nl-paragraph-font-family: var(--clippy-token-sample-text-font-family, var(--basis-text-font-family-default));
+    --nl-paragraph-color: var(--clippy-token-sample-text-color, var(--basis-color-default-color-document));
+  }
+
+  :host([truncate]) {
+    :where(.clippy-token-sample-typography__paragraph) {
+      -webkit-box-orient: vertical;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      overflow: clip;
+    }
+  }
 `;
