8b78c6e78e177e4e7fe9251286d3b606dcd7ca18
Bram Smulders <bram@supple.works>
2026-08-26 12:02:21 +0200

    wip: stylie stylings

--- packages/clippy-components/src/clippy-token-table/index.ts
+++ packages/clippy-components/src/clippy-token-table/index.ts
@@ -1,10 +1,20 @@
-import { safeCustomElement } from '@src/lib/decorators';
+import codeCss from '@nl-design-system-candidate/code-css/code.css?inline';
+import dataBadgeCss from '@nl-design-system-candidate/data-badge-css/data-badge.css?inline';
+import { BaseDesignToken, getTokenSubtype, stringifyToken } from '@nl-design-system-community/design-tokens-schema';
 import '../clippy-color-sample';
 import '../clippy-modal';
 import '../clippy-token-detail';
-import { LitElement, html } from 'lit';
+import { safeCustomElement } from '@src/lib/decorators';
+import { getTokenDimensionSpaceConcept, getTokenPath } from '@src/lib/tokens';
+import { LitElement, html, nothing, unsafeCSS } from 'lit';
+import { property } from 'lit/decorators.js';
 import srOnly from '../lib/sr-only';
 import styles from './styles';
+import '../clippy-color-sample';
+import '../clippy-token-sample-spacing';
+import '../clippy-token-sample-text';
+import '../clippy-token-sample-border';
+import '../clippy-graph-paper';
 
 const tag = 'clippy-token-table';
 
@@ -16,9 +26,134 @@ declare global {
 
 @safeCustomElement(tag)
 export class ClippyTokenTable extends LitElement {
-  static override readonly styles = [styles, srOnly];
+  static override readonly styles = [unsafeCSS(dataBadgeCss), unsafeCSS(codeCss), styles, srOnly];
+
+  @property({ type: Object })
+  tokens?: BaseDesignToken[];
+
+  @property({ attribute: 'example-label', type: String }) exampleLabel = 'Example';
+  @property({ attribute: 'token-id-label', type: String }) tokenIdLabel = 'Token ID';
+  @property({ attribute: 'value-label', type: String }) valueLabel = 'Value';
+  @property({ attribute: 'details-label', type: String }) detailsLabel = 'Details';
+
+  #renderTokenExample({ token }: { token: BaseDesignToken }) {
+    if (!token) {
+      return nothing;
+    }
+    switch (token.$type) {
+      case 'color':
+        return html`<clippy-color-sample color=${stringifyToken(token)}></clippy-color-sample>`;
+      case 'dimension': {
+        // dimensions have a lot of different subtypes
+        const subType = getTokenSubtype(token);
+        switch (subType) {
+          case 'font-size':
+            return html`<clippy-token-sample-text
+              font-size=${stringifyToken(token)}
+              truncate
+            ></clippy-token-sample-text>`;
+          case 'space-block':
+          case 'space-inline':
+          case 'space-text':
+          case 'space-column':
+          case 'space-row':
+            return html`<clippy-token-sample-spacing
+              size=${stringifyToken(token)}
+              concept=${getTokenDimensionSpaceConcept(token)}
+            ></clippy-token-sample-spacing>`;
+          case 'border-width':
+            return html`<clippy-token-sample-border
+              border-width=${stringifyToken(token)}
+            ></clippy-token-sample-border>`;
+          case 'border-radius':
+            return html`<clippy-token-sample-border
+              border-radius=${stringifyToken(token)}
+            ></clippy-token-sample-border>`;
+          default:
+            return nothing;
+        }
+      }
+      // TODO: Google fonts?
+      case 'fontFamily':
+        return html`<clippy-token-sample-text
+          font-family=${stringifyToken(token)}
+          font-size="var(--basis-text-font-size-xl)"
+          truncate
+        ></clippy-token-sample-text>`;
+      case 'number': {
+        const subType = getTokenSubtype(token);
+        switch (subType) {
+          case 'font-weight':
+            return html`<clippy-token-sample-text
+              font-weight=${stringifyToken(token)}
+              font-size="var(--basis-text-font-size-xl)"
+              truncate
+            ></clippy-token-sample-text>`;
+          case 'line-height':
+            return html`<clippy-token-sample-text
+              line-height=${stringifyToken(token)}
+              font-size="var(--basis-text-font-size-xl)"
+              truncate
+            ></clippy-token-sample-text>`;
+          default:
+            return nothing;
+        }
+      }
+      default:
+        return nothing;
+    }
+  }
 
   override render() {
-    return html`<mark>Hello world</mark>`;
+    return html`
+      <div role="table" class="clippy-token-table__table">
+        <div role="row" class="clippy-token-table__header">
+          <span role="columnheader" class="clippy-token-table__cell clippy-token-table__head">
+            ${this.exampleLabel}
+          </span>
+          <span role="columnheader" class="clippy-token-table__cell clippy-token-table__head">
+            ${this.tokenIdLabel}
+          </span>
+          <span role="columnheader" class="clippy-token-table__cell clippy-token-table__head">${this.valueLabel}</span>
+          <span role="columnheader" class="clippy-token-table__cell clippy-token-table__head">
+            ${this.detailsLabel}
+          </span>
+        </div>
+        <div role="rowgroup" class="clippy-token-table__body">
+          ${this.tokens?.map((token) => {
+            return html`
+              <div role="row" class="clippy-token-table__row">
+                <div class="clippy-token-table__cell">
+                  <span aria-hidden="true" class="clippy-token-table__head clippy-token-table__head--visual-small">
+                    ${this.exampleLabel}
+                  </span>
+                  <clippy-graph-paper class="clippy-token-table__example">
+                    ${this.#renderTokenExample({ token })}
+                  </clippy-graph-paper>
+                </div>
+                <div class="clippy-token-table__cell">
+                  <span aria-hidden="true" class="clippy-token-table__head clippy-token-table__head--visual-small">
+                    ${this.tokenIdLabel}
+                  </span>
+                  <span class="nl-data-badge">${getTokenPath(token)}</span>
+                </div>
+                <div class="clippy-token-table__cell">
+                  <span aria-hidden="true" class="clippy-token-table__head clippy-token-table__head--visual-small">
+                    ${this.valueLabel}
+                  </span>
+                  <code class="nl-code">${stringifyToken(token)}</code>
+                </div>
+                <div class="clippy-token-table__cell">
+                  <span aria-hidden="true" class="clippy-token-table__head clippy-token-table__head--visual-small">
+                    ${this.detailsLabel}
+                  </span>
+                  <mark>details</mark>
+                </div>
+              </div>
+            `;
+          })}
+        </div>
+      </div>
+    `;
   }
 }
--- packages/clippy-components/src/clippy-token-table/styles.ts
+++ packages/clippy-components/src/clippy-token-table/styles.ts
@@ -4,4 +4,98 @@ export default css`
   :host(:not([hidden])) {
     display: block;
   }
+
+  :host {
+    /*--clippy-graph-paper-line-color: red;*/
+
+    container: clippy-token-table / inline-size;
+    font-family: var(--basis-text-font-family-default);
+    font-size: var(--basis-text-font-size-md);
+    line-height: var(--basis-text-line-height-md);
+  }
+
+  .clippy-token-table__table {
+    display: grid;
+  }
+
+  .clippy-token-table__header {
+    @container clippy-token-table (inline-size < 64rem) {
+      position: absolute;
+      inline-size: 1px;
+      block-size: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0, 0, 0, 0);
+      white-space: nowrap;
+      border-width: 0;
+    }
+  }
+
+  .clippy-token-table__body {
+    display: grid;
+  }
+
+  .clippy-token-table__row {
+    display: grid;
+    row-gap: var(--basis-space-row-lg);
+    border-block-end-width: 1px;
+    border-block-end-color: var(--basis-color-default-border-subtle);
+    border-block-end-style: solid;
+    padding-block-end: var(--basis-space-row-2xl);
+
+    & + & {
+      padding-block-start: var(--basis-space-row-2xl);
+    }
+  }
+
+  .clippy-token-table__cell {
+  }
+
+  .clippy-token-table__head {
+    display: block;
+    font-weight: var(--basis-text-font-weight-bold);
+  }
+
+  .clippy-token-table__head--visual-small {
+  }
+
+  .clippy-token-table__example {
+    display: block;
+    padding-block: var(--basis-space-block-lg);
+    padding-inline: var(--basis-space-inline-lg);
+    border-width: 1px;
+    border-style: solid;
+    border-color: var(--basis-color-default-border-subtle);
+  }
+
+  @container clippy-token-table (inline-size >= 40rem) {
+    .clippy-token-table__table {
+      grid-template-columns: 1fr 1fr;
+      column-gap: var(--basis-space-column-4xl);
+    }
+
+    .clippy-token-table__header,
+    .clippy-token-table__body,
+    .clippy-token-table__row {
+      display: grid;
+      grid-template-columns: subgrid;
+      grid-column: 1 / -1;
+    }
+  }
+
+  @container clippy-token-table (inline-size >= 64rem) {
+    .clippy-token-table__table {
+      grid-template-columns: 1fr auto auto auto;
+    }
+
+    .clippy-token-table__head--visual-small {
+      display: none;
+    }
+
+    .clippy-token-table__row {
+      align-items: center;
+      padding-block-start: var(--basis-space-row-2xl);
+    }
+  }
 `;
