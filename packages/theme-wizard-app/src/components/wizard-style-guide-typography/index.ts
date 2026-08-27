51154d466d73951b0c252cf932986ce7309129cd
Bram Smulders <bram@supple.works>
2026-08-27 16:09:43 +0200

    wip: style-guide-typography uses new clippy-token-table

--- packages/theme-wizard-app/src/components/wizard-style-guide-typography/index.ts
+++ packages/theme-wizard-app/src/components/wizard-style-guide-typography/index.ts
@@ -1,4 +1,3 @@
-import type { DesignToken } from 'style-dictionary/types';
 import '@nl-design-system-community/clippy-components/clippy-html-image';
 import { consume } from '@lit/context';
 import codeCss from '@nl-design-system-candidate/code-css/code.css?inline';
@@ -6,28 +5,21 @@ import dataBadgeCss from '@nl-design-system-candidate/data-badge-css/data-badge.
 import '@nl-design-system-community/clippy-components/clippy-heading';
 import '@nl-design-system-community/clippy-components/clippy-toggletip';
 import '@nl-design-system-community/clippy-components/clippy-token-sample-text';
+import '@nl-design-system-community/clippy-components/clippy-token-table';
 import linkCss from '@nl-design-system-candidate/link-css/link.css?inline';
 import paragraphCss from '@nl-design-system-candidate/paragraph-css/paragraph.css?inline';
-import googleFonts from '@nl-design-system-community/clippy-components/assets/google-fonts.json' with { type: 'json' };
-import {
-  EXTENSION_REFERENCED_AT,
-  EXTENSION_TOKEN_PATH,
-  type ModernDimensionToken,
-} from '@nl-design-system-community/design-tokens-schema';
+import { TokenGroup } from '@nl-design-system-community/clippy-components/clippy-reset-theme';
 import ClipboardCopyIcon from '@tabler/icons/outline/clipboard-copy.svg?raw';
 import buttonLinkStyles from '@utrecht/link-button-css?inline';
 import tableCss from '@utrecht/table-css/dist/index.css?inline';
-import dlv from 'dlv';
-import { LitElement, html, nothing, unsafeCSS } from 'lit';
+import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import { html as staticHtml } from 'lit/static-html.js';
 import type Theme from '../../lib/Theme';
-import type { DisplayToken, FontFamilyToken, FontSizeToken } from '../wizard-style-guide/types';
 import { themeContext } from '../../contexts/theme';
 import { t } from '../../i18n';
 import styles from '../wizard-style-guide/styles';
-import { openTokenDialog, renderTokenDialog } from '../wizard-style-guide/utils';
 
 const tag = 'wizard-style-guide-typography';
 
@@ -43,141 +35,42 @@ export class WizardStyleGuideTypography extends LitElement {
   @state()
   private readonly theme!: Theme;
 
-  @state() private activeToken?: DisplayToken;
-
   static override readonly styles = [
+    /* Remove when all tables are clippy-token-table */
     unsafeCSS(dataBadgeCss),
     unsafeCSS(tableCss),
     unsafeCSS(codeCss),
+    unsafeCSS(buttonLinkStyles),
+    /* end-remove */
     unsafeCSS(linkCss),
     unsafeCSS(paragraphCss),
-    unsafeCSS(buttonLinkStyles),
     styles,
   ];
 
-  #linkToGoogleFontsSpecimen(family: string): string | null {
-    const googleFont = googleFonts.find((font) => font.label === family);
-    if (!googleFont) return null;
-    return `https://fonts.google.com/specimen/${googleFont.label.replaceAll(/\s+/g, '+')}`;
-  }
-
-  #prepareFontFamilies(text: Record<string, unknown>): FontFamilyToken[] {
-    return Object.entries(text['font-family'] as Record<string, unknown>).map(([name, tokenValue]) => {
-      const value = (tokenValue as DesignToken).$value;
-      const searchFamily = Array.isArray(value) ? value.at(0) : value;
-      const googleFontsSpecimen = this.#linkToGoogleFontsSpecimen(searchFamily);
-
-      const displayValue = Array.isArray(value) ? value.join(', ') : value;
-      const tokenId = (tokenValue && dlv(tokenValue, ['$extensions', EXTENSION_TOKEN_PATH])) ?? '';
-      const usage = (tokenValue && dlv(tokenValue, ['$extensions', EXTENSION_REFERENCED_AT])) ?? [];
-      const usageCount = usage.length;
-      return { name, displayValue, googleFontsSpecimen, tokenId, usage, usageCount };
-    });
-  }
-
-  #prepareFontSizes(text: Record<string, unknown>): FontSizeToken[] {
-    return Object.entries(text['font-size'] as Record<string, unknown>)
-      .reverse()
-      .map(([name, tokenValue]) => {
-        const { $value } = tokenValue as ModernDimensionToken;
-        const displayValue = $value.value?.toString() + $value.unit;
-        const tokenId = (tokenValue && dlv(tokenValue, ['$extensions', EXTENSION_TOKEN_PATH])) ?? '';
-        const usage = (tokenValue && dlv(tokenValue, ['$extensions', EXTENSION_REFERENCED_AT])) ?? [];
-        const usageCount = usage.length;
-        return { name, displayValue, tokenId, usage, usageCount };
-      });
-  }
-
-  #openDialog(token: DisplayToken) {
-    openTokenDialog(token, this.renderRoot, (t) => {
-      this.activeToken = t;
-    });
-  }
+  // #linkToGoogleFontsSpecimen(family: string): string | null {
+  //   const googleFont = googleFonts.find((font) => font.label === family);
+  //   if (!googleFont) return null;
+  //   return `https://fonts.google.com/specimen/${googleFont.label.replaceAll(/\s+/g, '+')}`;
+  // }
 
   override render() {
     const basis = this.theme.tokens['basis'] as Record<string, unknown>;
     const text = basis['text'] as Record<string, unknown>;
-    const fontFamilies = this.#prepareFontFamilies(text);
-    const fontSizes = this.#prepareFontSizes(text);
 
     return html`
       <div class="wizard-style-guide">
-        <wizard-table-scroller>
-          <table class="utrecht-table">
-            <caption class="utrecht-table__caption">
-              ${t('styleGuide.sections.typography.families.title')}
-            </caption>
-            <thead class="utrecht-table__header">
-              <tr class="utrecht-table__row">
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.sample')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.tokenName')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.value')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.details')}</th>
-              </tr>
-            </thead>
-            <tbody class="utrecht-table__body">
-              ${fontFamilies.map(
-                ({ name, displayValue, googleFontsSpecimen, tokenId, usage }) => html`
-                  <tr class="utrecht-table__row">
-                    <td class="utrecht-table__cell">
-                      <clippy-token-sample-text
-                        font-family=${displayValue}
-                        font-size="var(--basis-text-font-size-xl)"
-                        truncate
-                      ></clippy-token-sample-text>
-                      ${
-                        googleFontsSpecimen
-                          ? html`<p class="nl-paragraph">
-                              <a class="nl-link" href=${googleFontsSpecimen} rel="external noreferrer" target="_blank">
-                                ${t('tokens.showOnGoogleFonts')}
-                              </a>
-                            </p>`
-                          : nothing
-                      }
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <span class="nl-data-badge" id="${`basis-text-font-family-${name}`}">${tokenId}</span>
-                      <clippy-toggletip text=${t('copyToClipboard')}>
-                        <clippy-button
-                          icon-only
-                          purpose="subtle"
-                          size="small"
-                          @click=${() => navigator.clipboard.writeText(tokenId)}
-                        >
-                          ${t('copyValueToClipboard', { value: tokenId })}
-                          <clippy-icon size="small" slot="iconEnd">${unsafeSVG(ClipboardCopyIcon)}</clippy-icon>
-                        </clippy-button>
-                      </clippy-toggletip>
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <code class="nl-code">${displayValue}</code>
-                      <clippy-toggletip text=${t('copyToClipboard')}>
-                        <clippy-button
-                          icon-only
-                          purpose="subtle"
-                          size="small"
-                          @click=${() => navigator.clipboard.writeText(displayValue)}
-                        >
-                          ${t('copyValueToClipboard', { value: displayValue })}
-                          <clippy-icon size="small" slot="iconEnd">${unsafeSVG(ClipboardCopyIcon)}</clippy-icon>
-                        </clippy-button>
-                      </clippy-toggletip>
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <button
-                        type="button"
-                        class="utrecht-link-button utrecht-link-button--html-button"
-                        @click=${() => this.#openDialog({ displayValue, tokenId, tokenType: 'fontFamily', usage })}
-                      >
-                        ${t('styleGuide.showDetails')}
-                      </button>
-                    </td>
-                  </tr>
-                `,
-              )}
-            </tbody>
-          </table>
-        </wizard-table-scroller>
+        <clippy-heading level="3">${t('styleGuide.sections.typography.families.title')}</clippy-heading>
+        <clippy-token-table
+          .tokens=${Object.values(text['font-family'] as TokenGroup)}
+          example-label=${t('styleGuide.sample')}
+          token-id-label=${t('styleGuide.tokenName')}
+          value-label=${t('styleGuide.value')}
+          details-label=${t('styleGuide.details')}
+          show-details-label=${t('styleGuide.showDetails')}
+          copy-to-clipboard-label=${t('styleGuide.detailsDialog.copyToClipboard')}
+          reference-title-label=${t('styleGuide.detailsDialog.tokenReferenceList.title')}
+          reference-empty-label=${t('styleGuide.detailsDialog.tokenReferenceList.empty')}
+        ></clippy-token-table>
 
         <p class="nl-paragraph">
           <a
@@ -189,69 +82,19 @@ export class WizardStyleGuideTypography extends LitElement {
           </a>
         </p>
 
-        <wizard-table-scroller>
-          <table class="utrecht-table">
-            <caption class="utrecht-table__caption">
-              ${t('styleGuide.sections.typography.sizes.title')}
-            </caption>
-            <thead class="utrecht-table__header">
-              <tr class="utrecht-table__row">
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.sample')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.tokenName')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.value')}</th>
-                <th scope="col" class="utrecht-table__header-cell">${t('styleGuide.details')}</th>
-              </tr>
-            </thead>
-            <tbody class="utrecht-table__body">
-              ${fontSizes.map(
-                ({ name, displayValue, tokenId, usage }) => html`
-                  <tr class="utrecht-table__row">
-                    <td class="utrecht-table__cell">
-                      <clippy-token-sample-text font-size=${displayValue} truncate></clippy-token-sample-text>
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <span class="nl-data-badge" id="${`basis-text-font-size-${name}`}">${tokenId}</span>
-                      <clippy-toggletip text=${t('copyToClipboard')}>
-                        <clippy-button
-                          icon-only
-                          purpose="subtle"
-                          size="small"
-                          @click=${() => navigator.clipboard.writeText(tokenId)}
-                        >
-                          ${t('copyValueToClipboard', { value: tokenId })}
-                          <clippy-icon size="small" slot="iconEnd">${unsafeSVG(ClipboardCopyIcon)}</clippy-icon>
-                        </clippy-button>
-                      </clippy-toggletip>
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <code class="nl-code">${displayValue}</code>
-                      <clippy-toggletip text=${t('copyToClipboard')}>
-                        <clippy-button
-                          icon-only
-                          purpose="subtle"
-                          size="small"
-                          @click=${() => navigator.clipboard.writeText(displayValue)}
-                        >
-                          ${t('copyValueToClipboard', { value: displayValue })}
-                          <clippy-icon size="small" slot="iconEnd">${unsafeSVG(ClipboardCopyIcon)}</clippy-icon>
-                        </clippy-button>
-                      </clippy-toggletip>
-                    </td>
-                    <td class="utrecht-table__cell">
-                      <button
-                        type="button"
-                        class="utrecht-link-button utrecht-link-button--html-button"
-                        @click=${() => this.#openDialog({ displayValue, tokenId, tokenType: 'fontSize', usage })}
-                      >
-                        ${t('styleGuide.showDetails')}
-                      </button>
-                    </td>
-                  </tr>
-                `,
-              )}
-            </tbody>
-          </table>
-        </wizard-table-scroller>
+        <clippy-heading level="3">${t('styleGuide.sections.typography.sizes.title')}</clippy-heading>
+        <clippy-token-table
+          .tokens=${Object.values(text['font-size'] as TokenGroup)}
+          example-label=${t('styleGuide.sample')}
+          token-id-label=${t('styleGuide.tokenName')}
+          value-label=${t('styleGuide.value')}
+          details-label=${t('styleGuide.details')}
+          show-details-label=${t('styleGuide.showDetails')}
+          copy-to-clipboard-label=${t('styleGuide.detailsDialog.copyToClipboard')}
+          reference-title-label=${t('styleGuide.detailsDialog.tokenReferenceList.title')}
+          reference-empty-label=${t('styleGuide.detailsDialog.tokenReferenceList.empty')}
+        ></clippy-token-table>
+
         <p class="nl-paragraph">
           <a
             class="nl-link"
@@ -265,7 +108,7 @@ export class WizardStyleGuideTypography extends LitElement {
         <wizard-table-scroller>
           <table class="utrecht-table">
             <caption class="utrecht-table__caption">
-              ${t('styleGuide.sections.typography.headings.title')}
+              <mark>${t('styleGuide.sections.typography.headings.title')}</mark>
             </caption>
             <thead class="utrecht-table__header">
               <tr class="utrecht-table__row">
@@ -310,8 +153,6 @@ export class WizardStyleGuideTypography extends LitElement {
           <a class="nl-link" href="https://nldesignsystem.nl/heading/" target="_blank">docs</a>
         </p>
       </div>
-
-      ${renderTokenDialog(this.activeToken)}
     `;
   }
 }
