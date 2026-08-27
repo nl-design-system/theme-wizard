f11ea57c9fd774d9ff2ff5bf6e45c5d9762a6e8d
Bram Smulders <bram@supple.works>
2026-08-27 11:07:36 +0200

    wip: stories

--- packages/clippy-components/src/clippy-token-sample/index.ts
+++ packages/clippy-components/src/clippy-token-sample/index.ts
@@ -1,6 +1,13 @@
+import { BaseDesignToken, getTokenSubtype, stringifyToken } from '@nl-design-system-community/design-tokens-schema';
 import { safeCustomElement } from '@src/lib/decorators';
-import { LitElement, html } from 'lit';
+import { getTokenDimensionSpaceConcept } from '@src/lib/tokens';
+import { LitElement, html, nothing } from 'lit';
+import { property } from 'lit/decorators.js';
 import styles from './styles';
+import '../clippy-color-sample';
+import '../clippy-token-sample-spacing';
+import '../clippy-token-sample-text';
+import '../clippy-token-sample-border';
 
 const tag = 'clippy-token-sample';
 
@@ -19,7 +26,74 @@ declare global {
 export class ClippyTokenSample extends LitElement {
   static override readonly styles = [styles];
 
+  @property({ type: Object })
+  token?: BaseDesignToken;
+
   override render() {
-    return html`<mark>Hello world</mark>`;
+    if (!this.token) {
+      return nothing;
+    }
+    switch (this.token.$type) {
+      case 'color':
+        return html`<clippy-color-sample color=${stringifyToken(this.token)}></clippy-color-sample>`;
+      case 'dimension': {
+        // dimensions have a lot of different subtypes
+        const subType = getTokenSubtype(this.token);
+        switch (subType) {
+          case 'font-size':
+            return html`<clippy-token-sample-text
+              font-size=${stringifyToken(this.token)}
+              truncate
+            ></clippy-token-sample-text>`;
+          case 'space-block':
+          case 'space-inline':
+          case 'space-text':
+          case 'space-column':
+          case 'space-row':
+            return html`<clippy-token-sample-spacing
+              size=${stringifyToken(this.token)}
+              concept=${getTokenDimensionSpaceConcept(this.token)}
+            ></clippy-token-sample-spacing>`;
+          case 'border-width':
+            return html`<clippy-token-sample-border
+              border-width=${stringifyToken(this.token)}
+            ></clippy-token-sample-border>`;
+          case 'border-radius':
+            return html`<clippy-token-sample-border
+              border-radius=${stringifyToken(this.token)}
+            ></clippy-token-sample-border>`;
+          default:
+            return nothing;
+        }
+      }
+      // TODO: Google fonts?
+      case 'fontFamily':
+        return html`<clippy-token-sample-text
+          font-family=${stringifyToken(this.token)}
+          font-size="var(--basis-text-font-size-xl)"
+          truncate
+        ></clippy-token-sample-text>`;
+      case 'number': {
+        const subType = getTokenSubtype(this.token);
+        switch (subType) {
+          case 'font-weight':
+            return html`<clippy-token-sample-text
+              font-weight=${stringifyToken(this.token)}
+              font-size="var(--basis-text-font-size-xl)"
+              truncate
+            ></clippy-token-sample-text>`;
+          case 'line-height':
+            return html`<clippy-token-sample-text
+              line-height=${stringifyToken(this.token)}
+              font-size="var(--basis-text-font-size-xl)"
+              truncate
+            ></clippy-token-sample-text>`;
+          default:
+            return nothing;
+        }
+      }
+      default:
+        return nothing;
+    }
   }
 }
--- packages/clippy-storybook/src/web-components/clippy-token-sample.stories.tsx
+++ packages/clippy-storybook/src/web-components/clippy-token-sample.stories.tsx
@@ -1,13 +1,28 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import '@nl-design-system-community/clippy-components/clippy-token-sample';
+import {
+  borderRadiusFixture,
+  borderWidthFixture,
+  colorFixture,
+  spacingFixture,
+  textFontFamilyFixture,
+  textFontSizeFixture,
+  textFontWeightFixture,
+  textLineHeightFixture,
+} from '@nl-design-system-community/clippy-components/src/clippy-token-sample/fixtures.js';
 import readme from '@nl-design-system-community/clippy-components/src/clippy-token-sample/README.md?raw';
+import { BaseDesignToken } from '@nl-design-system-community/design-tokens-schema';
 import React from 'react';
 
-type TokenSampleStoryArgs = {};
+type TokenSampleStoryArgs = {
+  token: BaseDesignToken;
+};
 
 const meta = {
   id: 'clippy-token-sample',
-  args: {},
+  args: {
+    token: colorFixture,
+  },
   parameters: {
     docs: {
       description: {
@@ -17,7 +32,7 @@ const meta = {
   },
   render: (args: TokenSampleStoryArgs) => React.createElement('clippy-token-sample', args),
   tags: ['autodocs'],
-  title: 'Clippy/Token Sample',
+  title: 'Clippy/Tokens/Token Sample',
 } satisfies Meta<TokenSampleStoryArgs>;
 
 export default meta;
@@ -26,4 +41,46 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   name: 'Default',
+};
+
+export const Spacing: Story = {
+  args: {
+    token: spacingFixture,
+  },
+};
+
+export const FontSize: Story = {
+  args: {
+    token: textFontSizeFixture,
+  },
+};
+
+export const FontFamily: Story = {
+  args: {
+    token: textFontFamilyFixture,
+  },
+};
+
+export const FontWeight: Story = {
+  args: {
+    token: textFontWeightFixture,
+  },
+};
+
+export const LineHeight: Story = {
+  args: {
+    token: textLineHeightFixture,
+  },
+};
+
+export const BorderWidth: Story = {
+  args: {
+    token: borderWidthFixture,
+  },
+};
+
+export const BorderRadius: Story = {
+  args: {
+    token: borderRadiusFixture,
+  },
 };
