3debc22a6fcc764e32369f994ec3c22b2f33ff31
Bram Smulders <bram@supple.works>
2026-08-28 16:05:29 +0200

    wip: cleanup css imports

--- packages/theme-wizard-app/src/components/wizard-style-guide-colors/index.ts
+++ packages/theme-wizard-app/src/components/wizard-style-guide-colors/index.ts
@@ -1,15 +1,10 @@
 import { consume } from '@lit/context';
-import codeCss from '@nl-design-system-candidate/code-css/code.css?inline';
-import colorSampleCss from '@nl-design-system-candidate/color-sample-css/color-sample.css?inline';
-import dataBadgeCss from '@nl-design-system-candidate/data-badge-css/data-badge.css?inline';
 import '@nl-design-system-community/clippy-components/clippy-heading';
 import '@nl-design-system-community/clippy-components/clippy-toggletip';
 import linkCss from '@nl-design-system-candidate/link-css/link.css?inline';
 import paragraphCss from '@nl-design-system-candidate/paragraph-css/paragraph.css?inline';
 import '@nl-design-system-community/clippy-components/clippy-color-sample';
 import { TokenPath } from '@nl-design-system-community/clippy-components/clippy-reset-theme';
-import buttonLinkStyles from '@utrecht/link-button-css?inline';
-import tableCss from '@utrecht/table-css/dist/index.css?inline';
 import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import type Theme from '../../lib/Theme';
@@ -32,16 +27,7 @@ export class WizardStyleGuideColors extends LitElement {
   @state()
   private readonly theme!: Theme;
 
-  static override readonly styles = [
-    unsafeCSS(dataBadgeCss),
-    unsafeCSS(tableCss),
-    unsafeCSS(colorSampleCss),
-    unsafeCSS(codeCss),
-    unsafeCSS(paragraphCss),
-    unsafeCSS(linkCss),
-    unsafeCSS(buttonLinkStyles),
-    styles,
-  ];
+  static override readonly styles = [unsafeCSS(paragraphCss), unsafeCSS(linkCss), styles];
 
   override render() {
     const basis = this.theme.tokens['basis'] as Record<string, unknown>;
