f11df246c5a359235aeb50a8e7868fdb45dc0aab
Bram Smulders <bram@supple.works>
2026-09-17 16:27:55 +0200

    chore: update stylelint rules

--- packages/theme-wizard-app/.stylelintrc.mjs
+++ packages/theme-wizard-app/.stylelintrc.mjs
@@ -2,11 +2,11 @@ export default {
   extends: ['../../.stylelintrc.json'],
   ignoreFiles: ['**/*', '!**/*.ts'],
   rules: {
-    'custom-property-pattern': '^_?(ams|basis|theme|denhaag|utrecht|todo|ma|wizard|nl)-[a-z0-9-]+$',
+    'custom-property-pattern': '^_?(ams|basis|theme|denhaag|utrecht|todo|ma|wizard|nl|clippy)-[a-z0-9-]+$',
     'keyframes-name-pattern': '^(ams|basis|theme|utrecht|todo|--wizard)-[a-z0-9-]+$',
     'scss/dollar-variable-pattern': '^(ams|basis|theme|utrecht|todo)-[a-z0-9-]+$',
     'scss/percent-placeholder-pattern': '^(ams|basis|theme|utrecht|todo)-[a-z0-9-]+$',
-    'selector-class-pattern': '^(ams|basis|theme|denhaag|utrecht|todo|wizard|nl)-[a-z0-9_-]+$',
+    'selector-class-pattern': '^(ams|basis|theme|denhaag|utrecht|todo|ma|wizard|nl|clippy)-[a-z0-9_-]+$',
   },
   overrides: [
     {
