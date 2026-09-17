90d3d5f3fe289ee67e8dc7c35a4a27669263eede
Bram Smulders <bram@supple.works>
2026-09-17 14:52:07 +0200

    feat: query header size for use in application

--- packages/clippy-components/src/clippy-page-layout/index.ts
+++ packages/clippy-components/src/clippy-page-layout/index.ts
@@ -1,5 +1,6 @@
 import { safeCustomElement } from '@src/lib/decorators';
 import { LitElement, html } from 'lit';
+import { query } from 'lit/decorators.js';
 import styles from './styles';
 
 const tag = 'clippy-page-layout';
@@ -19,6 +20,40 @@ declare global {
 @safeCustomElement(tag)
 export class ClippyPageLayout extends LitElement {
   static override readonly styles = [styles];
+
+  @query('header') private headerElement!: HTMLElement;
+
+  #resizeObserver?: ResizeObserver;
+  #lastHeaderSize = 0;
+
+  override connectedCallback() {
+    super.connectedCallback();
+
+    this.#resizeObserver ??= new ResizeObserver((entries) => {
+      const box = entries[entries.length - 1].borderBoxSize[0];
+      this.#setHeaderHeight(box?.blockSize);
+    });
+
+    if (this.headerElement) {
+      this.#resizeObserver.observe(this.headerElement);
+    }
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.#resizeObserver?.disconnect();
+  }
+
+  protected override firstUpdated() {
+    this.#resizeObserver!.observe(this.headerElement);
+  }
+
+  #setHeaderHeight(height: number) {
+    if (height !== this.#lastHeaderSize) {
+      this.#lastHeaderSize = height;
+      this.style.setProperty('--clippy-page-layout-header-block-size', `${height}px`);
+    }
+  }
 
   override render() {
     return html`
--- packages/clippy-components/src/clippy-page-layout/styles.ts
+++ packages/clippy-components/src/clippy-page-layout/styles.ts
@@ -6,6 +6,13 @@ export default css`
   }
 
   :host {
+    /**
+     * 1. Variable to use troughout your application (eg. when the header is sticky)
+     *    Gets overwritten by JS in the component
+     */
+    --clippy-page-layout-header-block-size: 1lh; /* [1] */
+
+    display: grid;
     grid-template-rows: auto 1fr auto;
     min-block-size: 100vb;
     background-color: var(--clippy-page-layout-background-color, var(--basis-color-accent-1-bg-subtle));
