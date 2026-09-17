486e3c483a59c3fe047ca2bdc5e1cb7978d0f4b1
Bram Smulders <bram@supple.works>
2026-09-17 16:16:21 +0200

    default 0px

--- packages/clippy-components/src/clippy-page-layout/styles.ts
+++ packages/clippy-components/src/clippy-page-layout/styles.ts
@@ -10,7 +10,7 @@ export default css`
      * 1. Variable to use troughout your application (eg. when the header is sticky)
      *    Gets overwritten by JS in the component
      */
-    --clippy-page-layout-header-block-size: 1lh; /* [1] */
+    --clippy-page-layout-header-block-size: 0px; /* [1] */
 
     background-color: var(--clippy-page-layout-background-color, var(--basis-color-default-bg-document));
     container-name: clippy-page-layout;
